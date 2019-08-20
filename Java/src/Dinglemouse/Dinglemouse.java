4b67070cb944332c348f5cdb644064d899b8821f
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-08-20 20:54:42 +0200

    DinglemaouseTest

---
+++ Java/src/Dinglemouse/Dinglemouse.java
@@ -0,0 +1,60 @@
+package Dinglemouse;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Dinglemouse {
+	public static void main(String[] args) {
+		ArrayList<String> alphabets = new ArrayList<>(Arrays.asList("A", "B", "A", "D", "A"));
+		System.out.println(alphabets);
+		alphabets.removeAll(Collections.singleton("A"));
+		System.out.println(alphabets);
+
+		List<String> animals = Arrays.asList("cow", "cat", "fly", "bird", "spider", "dog", "fly", "dog", "spider", "cow", "horse", "bird", "cat", "goat", "bird", "fly", "cat", "bird", "goat");
+		List<String> autopsy = Arrays.asList("spider", "dog", "dog", "spider", "horse");
+		System.out.println(Dinglemouse.oldLadySwallows(animals));
+
+	}
+
+	public static List<String> oldLadySwallows(final List<String> animals) {
+		Map<String, Integer> mapAnimals = new HashMap<>() {{
+			put("fly", 0);
+			put("spider", 1);
+			put("bird", 2);
+			put("cat", 3);
+			put("dog", 4);
+			put("goat", 5);
+			put("cow", 6);
+			put("horse", 7);
+		}};
+		Map<Integer, String> revAnimals = new HashMap<>() {{
+			put(0, "fly");
+			put(1, "spider");
+			put(2, "bird");
+			put(3, "cat");
+			put(4, "dog");
+			put(5, "goat");
+			put(6, "cow");
+			put(7, "horse");
+		}};
+
+		List<Integer> intAnimals = new ArrayList<>();
+		for (String animal : animals) {
+			if (animal.equals("horse")) {
+				intAnimals.add(7);
+				break;
+			}
+			intAnimals.add(mapAnimals.get(animal));
+		}
+		List<Integer> stomachInt = new ArrayList<>();
+		for (int intAnimal : intAnimals) {
+			if (stomachInt.contains(intAnimal - 1)) {
+				stomachInt.removeAll(Collections.singleton((intAnimal - 1)));
+			}
+			if (!stomachInt.contains(intAnimal + 1)) {
+				stomachInt.add(intAnimal);
+			}
+		}
+		return stomachInt.stream().map(revAnimals::get).collect(Collectors.toList());
+	}
+}
--- Java/src/JomoPipi/JomoPipiTest.java
+++ Java/src/JomoPipi/JomoPipiTest.java
@@ -1,39 +1,39 @@
-package JomoPipi;
-
-import org.testng.annotations.Test;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.*;
-
-public class JomoPipiTest {
-	@Test
-	public void test0() {
-		char[][]a = new char[][]{{'a','b','c','d'},
-				{'1','2','3','4'},
-				{'c','o','d','e'},
-				{'b','l','a','h'}};
-
-		char[][]b = new char[][]{{'h','a','b','c'},
-				{'d','1','2','3'},
-				{'4','c','o','d'},
-				{'e','b','l','a'}};
-		assertEquals(Arrays.deepToString(b),Arrays.deepToString(JomoPipi.shift(a,1)));
-	}
-	@Test
-	public void test1() {
-		char[][]a = new char[][]{{'b','o','o','b'}};
-
-		char[][]b = new char[][]{{'o','b','b','o'}};
-		assertEquals(Arrays.deepToString(b),Arrays.deepToString(JomoPipi.shift(a,2)));
-	}
-	@Test
-	public void test2() {
-		char[][]a = new char[][]{{'m','m','m'},
-				{'y','e','a'}};
-
-		char[][]b = new char[][]{{'y','e','a'},
-				{'m','m','m'}};
-		assertEquals(Arrays.deepToString(b), Arrays.deepToString(JomoPipi.shift(a,3)));
-	}
-}
+//package JomoPipi;
+//
+//import org.testng.annotations.Test;
+//
+//import java.util.Arrays;
+//
+//import static org.junit.Assert.*;
+//
+//public class JomoPipiTest {
+//	@Test
+//	public void test0() {
+//		char[][]a = new char[][]{{'a','b','c','d'},
+//				{'1','2','3','4'},
+//				{'c','o','d','e'},
+//				{'b','l','a','h'}};
+//
+//		char[][]b = new char[][]{{'h','a','b','c'},
+//				{'d','1','2','3'},
+//				{'4','c','o','d'},
+//				{'e','b','l','a'}};
+//		assertEquals(Arrays.deepToString(b),Arrays.deepToString(JomoPipi.shift(a,1)));
+//	}
+//	@Test
+//	public void test1() {
+//		char[][]a = new char[][]{{'b','o','o','b'}};
+//
+//		char[][]b = new char[][]{{'o','b','b','o'}};
+//		assertEquals(Arrays.deepToString(b),Arrays.deepToString(JomoPipi.shift(a,2)));
+//	}
+//	@Test
+//	public void test2() {
+//		char[][]a = new char[][]{{'m','m','m'},
+//				{'y','e','a'}};
+//
+//		char[][]b = new char[][]{{'y','e','a'},
+//				{'m','m','m'}};
+//		assertEquals(Arrays.deepToString(b), Arrays.deepToString(JomoPipi.shift(a,3)));
+//	}
+//}
