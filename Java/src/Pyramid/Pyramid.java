3808c1d6ca6716222b77a49512bac76f6a18acb4
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-08-29 12:28:29 +0200

    Pyramid

---
+++ Java/src/Pyramid/Pyramid.java
@@ -0,0 +1,72 @@
+package Pyramid;
+
+public class Pyramid{
+	public static void main(String[] args) {
+		String test = "\u001F";
+		System.out.println(watchPyramidFromTheSide(test));
+		System.out.println("---------");
+		System.out.println(watchPyramidFromAbove(test));
+		System.out.println("---------");
+		System.out.println(countVisibleCharactersOfThePyramid(test));
+		System.out.println("---------");
+		System.out.println(countAllCharactersOfThePyramid(test));
+	}
+
+	public static String watchPyramidFromTheSide(String characters){
+		if(characters == null ) return null;
+		if(characters.length() == 0) return "";
+		StringBuilder out = new StringBuilder();
+		characters = new StringBuilder(characters).reverse().toString();
+		int length = characters.length() * 2 - 1;
+		for (int i = 0; i <= characters.length() - 1; i++) {
+			StringBuilder part = new StringBuilder(" ".repeat(length));
+			String bricks = String.valueOf(characters.charAt(i)).repeat(((i + 1) * 2) - 1);
+			part.replace((length - bricks.length()) / 2, (length + bricks.length()) / 2, bricks);
+			out.append(part).append("\n");
+		}
+		out.deleteCharAt(out.length() - 1);
+		return out.toString();
+	}
+
+	public static String watchPyramidFromAbove(String characters){
+		if(characters == null ) return null;
+		if(characters.length() == 0) return "";
+		int length = characters.length() * 2 - 1;
+		char[][] above = new char[length][length];
+		int i = 0;
+		while (i < characters.length()){
+			int start = i;
+			int stop = length - i;
+			for (int y = 0; y < length; y++) {
+				for (int x = 0; x < length ; x++) {
+					if (y >= start && y < stop && x >= start && x < stop) {
+						above[y][x] = characters.charAt(i);
+					}
+				}
+			}
+			i++;
+		}
+		StringBuilder out = new StringBuilder();
+		for (char[] row : above) {
+			out.append(row).append("\n");
+		}
+		return out.deleteCharAt(out.length() - 1).toString();
+	}
+
+	public static int countVisibleCharactersOfThePyramid(String characters){
+		if(characters == null || characters.length() == 0) return -1;
+
+		return (int) Math.pow((characters.length() * 2 - 1), 2);
+	}
+
+	public static int countAllCharactersOfThePyramid(String characters){
+		if(characters == null || characters.length() == 0) return -1;
+		int length = characters.length() * 2 - 1;
+		int out = 0;
+		while (length > 0) {
+			out += Math.pow(length, 2);
+			length -= 2;
+		}
+		return out;
+	}
+}
---
+++ Java/src/Pyramid/SolutionTest.java
@@ -0,0 +1,53 @@
+package Pyramid;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+	@Test
+	public void basicTest2() {
+		String characters = "*#";
+		String expectedWatchFromSide =
+				" # \n" +
+						"***";
+		String expectedWatchFromAbove =
+				"***\n" +
+						"*#*\n" +
+						"***";
+		String actualWatchFromSide = Pyramid.watchPyramidFromTheSide(characters);
+		String actualWatchFromAbove = Pyramid.watchPyramidFromAbove(characters);
+		visualisation(expectedWatchFromSide, expectedWatchFromAbove, actualWatchFromSide, actualWatchFromAbove);
+		assertEquals(9, Pyramid.countVisibleCharactersOfThePyramid(characters));
+		assertEquals(10, Pyramid.countAllCharactersOfThePyramid(characters));
+	}
+
+	@Test
+	public void basicTest3() {
+		String characters = "abc";
+		String expectedWatchFromSide =
+				"  c  \n" +
+						" bbb \n" +
+						"aaaaa";
+		String expectedWatchFromAbove =
+				"aaaaa\n" +
+						"abbba\n" +
+						"abcba\n" +
+						"abbba\n" +
+						"aaaaa";
+		String actualWatchFromSide = Pyramid.watchPyramidFromTheSide(characters);
+		String actualWatchFromAbove = Pyramid.watchPyramidFromAbove(characters);
+		visualisation(expectedWatchFromSide, expectedWatchFromAbove, actualWatchFromSide, actualWatchFromAbove);
+		assertEquals(25, Pyramid.countVisibleCharactersOfThePyramid(characters));
+		assertEquals(35, Pyramid.countAllCharactersOfThePyramid(characters));
+	}
+
+	private void visualisation(String expectedWatchFromSide, String expectedWatchFromAbove, String actualWatchFromSide, String actualWatchFromAbove) {
+		System.out.println("From side correct:\n" + expectedWatchFromSide);
+		System.out.println("From above correct:\n" + expectedWatchFromAbove);
+		System.out.println("From side yours:\n" + actualWatchFromSide);
+		System.out.println("From above yours:\n" + actualWatchFromAbove);
+		assertEquals(expectedWatchFromSide, actualWatchFromSide);
+		assertEquals(expectedWatchFromAbove, actualWatchFromAbove);
+	}
+}
