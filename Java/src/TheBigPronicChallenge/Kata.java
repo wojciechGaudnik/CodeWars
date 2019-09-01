ebb18b3ec738a5483fec0d670db346017e656b75
root <root@localhost.localdomain>
2019-09-01 16:48:41 +0200

    TheBigPronicChallenge

---
+++ Java/src/TheBigPronicChallenge/Kata.java
@@ -0,0 +1,27 @@
+package TheBigPronicChallenge;
+
+import java.util.regex.Matcher;
+
+public class Kata {
+
+	public static void main(String[] args) {
+		System.out.print("Please enter a number: ");
+		long number = 1;
+
+		long n = (long)Math.sqrt(number);
+		if(n*(n+1)==number) {
+			System.out.println(number+" is a pronic number");
+		}else {
+			System.out.println(number+" is NOT a pronic number");
+		}
+
+		System.out.println(isPronic(number));
+	}
+
+
+	public static boolean isPronic(long n){
+		long nn = (long) Math.sqrt(n);
+		return n == (nn * (nn + 1));
+	}
+
+}
