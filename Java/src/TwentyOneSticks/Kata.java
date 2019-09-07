14aba3dabccbff40264129a2078cb0ed87982133
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-09-07 13:08:58 +0200

    21Sticks

---
+++ Java/src/TwentyOneSticks/Kata.java
@@ -0,0 +1,7 @@
+package TwentyOneSticks;
+
+public class Kata {
+	public static int makeMove(int sticks) {
+		return (sticks <= 3)?sticks:sticks%4;
+	}
+}
---
+++ Java/src/TwentyOneSticks/SolutionTest.java
@@ -0,0 +1,12 @@
+package TwentyOneSticks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+	@Test
+	public void sampleTest() {
+		assertEquals(3, Kata.makeMove(3));
+	}
+}
