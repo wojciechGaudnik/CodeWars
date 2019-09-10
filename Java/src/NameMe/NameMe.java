eddc70e6be5a3ab09b1ed42bc124f7e294063a79
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-09-10 19:33:35 +0200

    MyName

---
+++ Java/src/NameMe/NameMe.java
@@ -0,0 +1,26 @@
+package NameMe;
+
+
+public class NameMe {
+	public String firstName;
+	public String lastName;
+	public String fullName;
+
+	public NameMe(String first, String last) {
+		this.firstName = first;
+		this.lastName = last;
+		this.fullName = firstName + " " + lastName;
+	}
+
+	public String getFullName(){
+		return fullName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+}
---
+++ Java/src/NameMe/NameMeTest.java
@@ -0,0 +1,29 @@
+package NameMe;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class NameMeTest extends TestCase {
+
+	public void testNameMeOne() throws Exception {
+		NameMe nameMe = new NameMe("John", "Doe");
+
+		assertEquals("Verifying First name. Object returned should contain the firstname: John",
+				"John",
+				nameMe.getFirstName());
+
+		assertEquals("Verifying Last name. Object returned should contain the lastname: Doe",
+				"Doe",
+				nameMe.getLastName());
+
+		assertNotEquals("Verifying full name. Object returned should contain full name: John Doe",
+				"JohnDoe",
+				nameMe.getFullName());
+
+		assertEquals("Verifying full name. Object returned should contain full name: John Doe",
+				"John Doe",
+				nameMe.getFullName());
+	}
+}
