8c4fe64945669fc9a7ee3f38fee47aa16e8f82ed
root <root@localhost.localdomain>
2019-10-16 23:09:35 +0200

    All for simple Katas

---
+++ Java/src/All/Kata.java
@@ -0,0 +1,24 @@
+package All;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Kata{
+
+	public static void main(String[] args) {
+		int[] input = new int[] { 4, 3, 9, 7, 2, 1 };
+		System.out.println(Arrays.toString(squareOrSquareRoot(input)));
+	}
+
+	public static int[] squareOrSquareRoot(int[] array)	{
+		return Arrays.stream(array).boxed().map(n -> {
+			if ((Math.sqrt(n) - Math.floor(Math.sqrt(n))) == 0) {
+				System.out.println(n);
+				return (int)Math.sqrt(n);
+			} else {
+				return (int)Math.pow(n, 2);
+			}
+		}).mapToInt(Integer::intValue).toArray();
+	}
+}
--- Java/src/FunWithListsLength/Solution.java
+++ Java/src/FunWithListsLength/Solution.java
@@ -2,17 +2,85 @@ package FunWithListsLength;
 
 import lombok.experimental.Helper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 class Solution {
 
 	public static void main(String[] args) {
-		Node node = new Node(1);
-		node.next = new Node(2);
-		node.next.next = new Node(2);
+		Node node = new Node("aaa");
+		node.next = new Node("b");
+		node.next.next = new Node("abc");
 		System.out.println(length(node));
+		System.out.println(lastIndexOf(null, "b"));
+		System.out.println(weatherInfo(21));
+		System.out.println(weatherInfo(50).equals("10.0 is above freezing temperature"));
+		System.out.println(5/9.0);
+//				-11.66666666666666[6]
+//				-11.66666666666666[8]
+		System.out.println(find_average(new int[]{1, 2, 3}));
 	}
-	static int length(Node head) {
 
+	public static double find_average(int[] array){
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return (double) sum / array.length;
+
+	}
+
+	public static String weatherInfo(int temp) {
+		System.out.println(temp);
+		double c = convertToCelsius(temp);
+		if (c < 0)
+			return (c + " is freezing temperature");
+		else
+			return (c + " is above freezing temperature");
+	}
+
+	public static double convertToCelsius(int temperature) {
+		return (temperature - 32) * 0.5555555555555556;
+	}
+
+	static int lastIndexOf(Node head, Object value) {
+		List<Object> objectList = new ArrayList<>();
+		Node temp = head;
+		while (temp != null) {
+			objectList.add(temp.data);
+			temp = temp.next;
+		}
+		return objectList.lastIndexOf(value);
+	}
+
+
+	static int length(Node head) {
 		return (head == null)? 0:length(head.next) + 1;
+	}
+
+	static int indexOf(Node head, Object value) {
+		if (ifExist(head, value) == 0) {
+			if (head == null) {
+				return -1;
+			} else if (head.data.equals(value)) {
+				return 0;
+			} else {
+				return 1 + indexOf(head.next, value);
+			}
+		} else {
+			return -1;
+		}
+	}
+
+	private static int ifExist(Node head, Object value) {
+		if (head == null) {
+			return -1;
+		} else if (!head.data.equals(value)){
+			return ifExist(head.next, value);
+		};
+		return 0;
 	}
 }
 
