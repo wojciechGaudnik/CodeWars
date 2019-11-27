892726f76309d858f649e5174cf546a6f95dced8
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-11-27 22:02:06 +0100

    init

---
+++ C#/Katas/Katas/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Katas
+{
+    class Kata
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(GenerateRange(1,2,3));
+        }
+
+        public static int[] GenerateRange(int min, int max, int step)
+        {
+            return new int[]{};
+        }
+    }
+}
