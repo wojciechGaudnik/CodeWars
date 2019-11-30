7973d64493b1c155d3756491efd07ffed1b36e88
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-11-30 13:25:36 +0100

    anext

--- C#/Katas.Tests/Katas.Tests/UnitTest1.cs
+++ C#/Katas.Tests/Katas.Tests/UnitTest1.cs
@@ -3,11 +3,29 @@ using System;
 using Katas;
 
 [TestFixture]
-public class HackMyTerminalTests
+public class VaultPopulationTests
 {
     [Test]
-    public void PasswordWith4Letters() {
+    public void MankindIsExtinct()
+    {
+        var dwellers = VaultExperience_3__PopulateTheVaults.PopulateMyVault(0);
 
-        Assert.AreEqual("PILZ", VaultExperience_2__HackMyTerminal_.HackMyTerminal( 4, "/§TTE§*%RAUM*+?=)PILZ(/&A..-.%BAUM_:"), "Password incorrekt. Terminal has been locked!");
+//        VaultTec.AssertDwellersInAVault(dwellers, 0, 0, 0, 0);
+    }
+
+    [Test]
+    public void TenLittleDwellersInAVault()
+    {
+        var dwellers = VaultExperience_3__PopulateTheVaults.PopulateMyVault(10);
+
+//        VaultTec.AssertDwellersInAVault(dwellers, 10, 5, 5, 1);
+    }
+
+    [Test]
+    public void TwentyThreeDwellersInAVault()
+    {
+        var dwellers = VaultExperience_3__PopulateTheVaults.PopulateMyVault(23);
+
+//        VaultTec.AssertDwellersInAVault(dwellers, 23, 12, 11, 1);
     }
 }
---
+++ C#/Katas/Katas/VaultExperience(3):PopulateTheVaults.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+
+namespace Katas
+{
+    public class VaultExperience_3__PopulateTheVaults
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static List<Dweller> PopulateMyVault(int countOfDweller)
+        {
+            List<Dweller> dwellers = new List<Dweller>();
+            if (countOfDweller == 0) return dwellers;
+            int men = 0;
+            int women = 0;
+            dwellers.Add(new Dweller(Gender.Mr, Position.overseer));
+            men++;
+            if (countOfDweller > 50)
+            {
+                dwellers.Add(new Dweller(Gender.Mrs, Position.overseer));
+                women++;
+            }
+            while (dwellers.Count < countOfDweller)
+            {
+                if (men >= women)
+                {
+                    dwellers.Add(new Dweller(Gender.Mrs, Position.none));
+                    women++;
+                }
+                else
+                {
+                    dwellers.Add(new Dweller(Gender.Mr, Position.none));
+                    men++;
+                }
+            }
+            return dwellers;
+        }
+    }
+
+    public class Dweller
+    {
+        public Gender Sex { get; set; }
+
+        public Position Work { get; set; }
+
+        public Dweller() : this(Gender.Mr, Position.none) { }
+
+        public Dweller(Gender sex) : this(sex, Position.none) { }
+
+        public Dweller(Gender sex, Position work)
+        {
+            Sex = sex;
+            Work = work;
+        }
+    }
+
+    public enum Gender
+    {
+        Mr,
+        Mrs
+    }
+
+    public enum Position
+    {
+        overseer,
+        none
+    }
+}
