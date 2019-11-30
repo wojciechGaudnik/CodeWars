95683599121a6bcb0c496107cf8fb53ed715e028
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-11-30 17:10:25 +0100

    anext

--- C#/Katas.Tests/Katas.Tests/UnitTest1.cs
+++ C#/Katas.Tests/Katas.Tests/UnitTest1.cs
@@ -1,19 +1,74 @@
+using System;
 using Katas;
 using NUnit.Framework;
 
-namespace Tests
+[TestFixture]
+public class Examples
 {
-    public class Tests
+    [Test,Description("5 Example Tests")]
+    public static void ExampleTests()
     {
-        [TestCase("", 0)]
-        [TestCase("hello", 8)]
-        [TestCase("bacon(d)", 18)]
-        [TestCase("pizza(t)", 75)]
-        [TestCase("pupp**ie*s", 70)]
-        [TestCase("task^", 3)]
-        public void ExampleTests(string word, int expectedScore)
-        {
-            Assert.AreEqual(expectedScore, ScrabbleWordCalculator.CalculateScrabbleWord(word));
-        }
+        var example_test = new string[5][]{
+            new string[]{
+                "000001",
+                "010010",
+                "101001",
+                "010100",
+                "001010",
+                "010000",
+                "100010",
+                "010100",},
+            new string[]{
+                "01000100",
+                "00101000",
+                "01010101",
+                "10000000",
+                "10000100",
+                "10001000",
+                "00001010",
+                "01001000"},
+            new string[]{
+                "00000100",
+                "01000101",
+                "00001001",
+                "10000010",
+                "01001000",
+                "00101010",
+                "10001010",
+                "10101001"},
+            new string[]{
+                "00010000",
+                "10000100",
+                "10100010",
+                "10010100",
+                "10101001",
+                "01000000",
+                "01000100",
+                "00100001",
+                "00000100",
+                "01000010"},
+            new string[]{
+                "0010001010",
+                "1001000000",
+                "0100010010",
+                "0101001000",
+                "0100100001",
+                "1001001010",
+                "0100101010",
+                "1001001000",
+                "1000101001",
+                "0001000100"}
+        };
+
+        var example_sols = new int[5][]{
+            new int[]{6,3,2,1,0,5,4},
+            new int[]{3,2,0,6,4,1,8,5,7},
+            new int[]{2,1,0,3,5,4,7,8,6},
+            new int[]{0,4,1,3,7,2,5,6,8},
+            new int[]{3,0,1,2,4,7,6,8,5,9,10}
+        };
+
+        for (int i = 0; i < 5; i++){
+            Assert.That(Banzai.Amidakuji(example_test[i]),Is.EqualTo(example_sols[i]));}
     }
 }
---
+++ C#/Katas/Katas/Amidakuji.cs
@@ -0,0 +1,32 @@
+namespace Katas
+{
+    public class Banzai
+    {
+        public static int[] Amidakuji(string[] ar)
+        {
+            int[] answer = new int[ar[0].Length + 1];
+            for (int i = 0; i < ar.Length; i++)ar[i] = "0" + ar[i] + "0";
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                int position = i + 1;
+                for (int j = 0; j < ar.Length; j++)
+                {
+                    if (ar[j][position - 1] == '1')
+                    {
+                        position--;
+                        continue;
+                    }
+                    if (ar[j][position] == '1')position++;
+                }
+                answer[position-1] = i;
+            }
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
