6490c94c196f46a80c102b9fb9fb241ab4805e6c
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-12-07 10:53:19 +0100

    Next

--- C#/Katas/Katas/FinancingPlanOnPlanetXY140Z-n.cs
+++ C#/Katas/Katas/FinancingPlanOnPlanetXY140Z-n.cs
@@ -1,30 +1,7 @@
-using System;
-using System.Linq;
-
 namespace Katas
 {
     public class FinancingPlanOnPlanetXY140Z_n
     {
-
-
-        static void Main(string[] args)
-        {
-            int[] test = {1,2,3,4,};
-            test.Select(x => x).ToList().ForEach(x => Console.Write(x + ", "));
-            int x = test[0];
-            x = 10;
-            Console.WriteLine();
-            test.Select(x => x).ToList().ForEach(x => Console.Write(x + ", "));
-            ref int x2 =ref test[0];
-            x2 = 10;
-            Console.WriteLine();
-            test.Select(x => x).ToList().ForEach(x => Console.Write(x + ", "));
-            Console.WriteLine(finance(1));
-            Console.WriteLine(finance(5000));
-
-
-        }
-
         public static ulong finance(ulong n)
         {
             ulong answer = 0, last = 0, top = 1, bottom = 1;
--- C#/Katas/Katas/FoldAnArray.cs
+++ C#/Katas/Katas/FoldAnArray.cs
@@ -1,4 +1,30 @@
-$HEADER$namespace $NAMESPACE$
+using System.Collections;
+
+namespace Katas
 {
-  public class $CLASS$ {$END$}
+    public class FoldAnArray
+    {
+        public static int[] FoldArray(int[] array, int runs)
+        {
+            ArrayList answer = new ArrayList();
+            answer.AddRange(array);
+
+            while (runs-- > 0)
+            {
+                int length = answer.Count / 2;
+                for (int i = 0; i < length; i++)
+                {
+                    answer[i] = (int)answer[i] + (int)answer[answer.Count - 1];
+                    answer.RemoveAt(answer.Count - 1);
+                }
+            }
+            int[] outAnswer = new int[answer.Count];
+            int j = 0;
+            foreach (int n in answer)
+            {
+                outAnswer[j++] = n;
+            }
+            return outAnswer;
+        }
+    }
 }
