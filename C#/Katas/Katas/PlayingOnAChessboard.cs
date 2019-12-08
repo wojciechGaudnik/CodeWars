db6f6a0d0820797487b6795589ca81638cbab3e1
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-12-08 13:02:46 +0100

    Next

--- C#/Katas/Katas/PlayingOnAChessboard.cs
+++ C#/Katas/Katas/PlayingOnAChessboard.cs
@@ -2,15 +2,23 @@ using System;
 
 public class PlayingOnAChessboard
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine();
-    }
     public static string game(long n)
     {
         Console.WriteLine(n);
         if (n == 0) return "[0]";
         if (n % 2 == 0) return ($"[{n * (n / 2)}]");
         return $"[{n * n}, 2]";
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(pageCount(5, 4));
+    }
+
+    static int pageCount(int n, int p)
+    {
+        if (n % 2 == 0) n++;
+        if (p % 2 == 0) p++;
+        return (p - 1 < n - p)? (p - 1) / 2: (n - p) / 2;
     }
 }
