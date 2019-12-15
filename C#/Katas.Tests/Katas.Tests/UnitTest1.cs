05f70d3aebb599b98905c5e007646cc75d74ec02
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-12-15 11:16:28 +0100

    next

--- C#/Katas.Tests/Katas.Tests/UnitTest1.cs
+++ C#/Katas.Tests/Katas.Tests/UnitTest1.cs
@@ -4,30 +4,26 @@ namespace myjinxin
 {
     using NUnit.Framework;
     using System;
-    
+
     [TestFixture]
     public class myjinxin
     {
-        
+
         [Test]
         public void BasicTests(){
-            var SimpleFun_158_NotSoRandom=new SimpleFun_158_NotSoRandom();
-            
-            Assert.AreEqual("Black",SimpleFun_158_NotSoRandom.NotSoRandom(1,1));
-            
-            Assert.AreEqual("White",SimpleFun_158_NotSoRandom.NotSoRandom(2,1));
-            
-            Assert.AreEqual("Black",SimpleFun_158_NotSoRandom.NotSoRandom(1,2));
-            
-            Assert.AreEqual("White",SimpleFun_158_NotSoRandom.NotSoRandom(2,2));
-            
-            Assert.AreEqual("White",SimpleFun_158_NotSoRandom.NotSoRandom(6,9));
-            
-            Assert.AreEqual("White",SimpleFun_158_NotSoRandom.NotSoRandom(10000,10000));
-            
-            Assert.AreEqual("Black",SimpleFun_158_NotSoRandom.NotSoRandom(11111,22222));
-            
+            var kata=new SimpleFun_162_PairWise();
+
+            Assert.AreEqual(11,kata.Pairwise(new int[]{1, 4, 2, 3, 0, 5},7));
+
+            Assert.AreEqual(1,kata.Pairwise(new int[]{1, 3, 2, 4},4));
+
+            Assert.AreEqual(1,kata.Pairwise(new int[]{1, 1, 1},2));
+
+            Assert.AreEqual(10,kata.Pairwise(new int[]{0, 0, 0, 0, 1, 1},1));
+
+            Assert.AreEqual(0,kata.Pairwise(new int[]{15, 1, 1},5));
+
         }
-                 
+
     }
 }
--- C#/Katas/Katas/SimpleFun#158:NotSoRandom.cs
+++ C#/Katas/Katas/SimpleFun#158:NotSoRandom.cs
@@ -1,14 +1,7 @@
-using System;
-
 namespace Katas
 {
     public class SimpleFun_158_NotSoRandom
     {
-        static void Main(string[] args)
-        {
-
-        }
-
         public string NotSoRandom(double b, double w)
         {
             return ((uint)b % 2 == 1)?"Black":"White";
