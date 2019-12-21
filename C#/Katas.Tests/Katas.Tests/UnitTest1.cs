4983ec71c8c15ea02003969f81ed1d7a48d7d511
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-12-21 16:03:30 +0100

    next

--- C#/Katas.Tests/Katas.Tests/UnitTest1.cs
+++ C#/Katas.Tests/Katas.Tests/UnitTest1.cs
@@ -1,18 +1,19 @@
+using NUnit.Framework;
+using System;
 using Katas;
 
-namespace Solution
+[TestFixture]
+public class IQTests
 {
-    using NUnit.Framework;
-    using System;
-
-    [TestFixture]
-    public class SolutionTest
+    [Test]
+    public void Test1()
     {
-        [Test]
-        public void SampleTest()
-        {
-            Assert.AreEqual("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", ReplaceWithAlphabetPosition.AlphabetPosition("The sunset sets at twelve o' clock."));
-            Assert.AreEqual("20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20", ReplaceWithAlphabetPosition.AlphabetPosition("The narwhal bacons at midnight."));
-        }
+        Assert.AreEqual(3, IQTest.Test("2 4 7 8 10"));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(1, IQTest.Test("1 2 2"));
     }
 }
---
+++ C#/Katas/Katas/IQTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+namespace Katas
+{
+    public class IQTest
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Test("2 4 7 8 10"));
+            Console.WriteLine(Test("1 2 1 1"));
+        }
+
+        public static int Test(string word)
+        {
+            var array = word.Split(" ").Select(w => int.Parse(w)).ToArray();
+            var search = 0;
+            if (array[0] % 2 == 0 && array[1] % 2 == 0) search = 1;
+            if (array[0] % 2 == 0 && array[1] % 2 == 1) search = (array[2] % 2 == 1)? 0 : 1;
+            if (array[0] % 2 == 1 && array[1] % 2 == 0) search = (array[2] % 2 == 1)? 0 : 1;
+            foreach (var i in array)
+            {
+                if(i % 2 == search) return Array.IndexOf(array, i) + 1;
+            }
+
+            return -1;
+        }
+    }
+}
