f21dd5616cc33cb16b1364711406997eb4c96056
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-01-06 11:48:58 +0100

    next

--- C#/Katas.Tests/Katas.Tests/UnitTest1.cs
+++ C#/Katas.Tests/Katas.Tests/UnitTest1.cs
@@ -1,46 +1,38 @@
 using Katas;
+using NUnit.Framework;
 
-namespace Solution {
-  using NUnit.Framework;
-  using System;
+public class SolutionTest
+{
 
-  [TestFixture]
-  public class SolutionTest
-  {
-    private string[] urls = new string[] {"mysite.com/pictures/holidays.html",
-                                          "www.codewars.com/users/GiacomoSorbi?ref=CodeWars",
-                                          "www.microsoft.com/docs/index.htm#top",
-                                          "mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp",
-                                          "www.very-long-site_name-to-make-a-silly-yet-meaningful-example.com/users/giacomo-sorbi",
-                                          "https://www.linkedin.com/in/giacomosorbi",
-                                          "www.agcpartners.co.uk/",
-                                          "www.agcpartners.co.uk",
-                                          "https://www.agcpartners.co.uk/index.html",
-                                          "http://www.agcpartners.co.uk"};
-
-    private string[] seps = new string[] {" : ", " / ", " * ", " > ", " + ", " * ", " * ", " # ", " >>> ", " % "};
-
-
-    private string[] anss = new string[] {"<a href=\"/\">HOME</a> : <a href=\"/pictures/\">PICTURES</a> : <span class=\"active\">HOLIDAYS</span>",
-                                          "<a href=\"/\">HOME</a> / <a href=\"/users/\">USERS</a> / <span class=\"active\">GIACOMOSORBI</span>",
-                                          "<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>",
-                                          "<a href=\"/\">HOME</a> > <a href=\"/very-long-url-to-make-a-silly-yet-meaningful-example/\">VLUMSYME</a> > <span class=\"active\">EXAMPLE</span>",
-                                          "<a href=\"/\">HOME</a> + <a href=\"/users/\">USERS</a> + <span class=\"active\">GIACOMO SORBI</span>",
-                                          "<a href=\"/\">HOME</a> * <a href=\"/in/\">IN</a> * <span class=\"active\">GIACOMOSORBI</span>",
-                                          "<span class=\"active\">HOME</span>",
-                                          "<span class=\"active\">HOME</span>",
-                                          "<span class=\"active\">HOME</span>",
-                                          "<span class=\"active\">HOME</span>"};
     [Test]
-    public void ExampleTests()
+    public void sampleTests()
     {
-      for (int i = 0; i < 10; i++)
-      {
-        Console.WriteLine($"\nTest With: {urls[i]}");
-        if (i == 5) Console.WriteLine("\nThe one used in the above test was my LinkedIn Profile; if you solved the kata this far and manage to get it, feel free to add me as a contact, message me about the language that you used and I will be glad to endorse you in that skill and possibly many others :)\n\n ");
 
-        Assert.AreEqual(anss[i], BreadcrumbGenerator.GenerateBC(urls[i], seps[i]));
-      }
+        string a = ".W.\n" +
+                   ".W.\n" +
+                   "...",
+
+            b = ".W.\n" +
+                ".W.\n" +
+                "W..",
+
+            c = "......\n" +
+                "......\n" +
+                "......\n" +
+                "......\n" +
+                "......\n" +
+                "......",
+
+            d = "......\n" +
+                "......\n" +
+                "......\n" +
+                "......\n" +
+                ".....W\n" +
+                "....W.";
+
+        Assert.AreEqual(true, PathFinder_1CanYouReachTheExit.PathFinder(a));
+        Assert.AreEqual(false, PathFinder_1CanYouReachTheExit.PathFinder(b));
+        Assert.AreEqual(true, PathFinder_1CanYouReachTheExit.PathFinder(c));
+        Assert.AreEqual(false, PathFinder_1CanYouReachTheExit.PathFinder(d));
     }
-  }
 }
---
+++ C#/Katas/Katas/PathFinder#1CanYouReachTheExit.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Linq;
+
+namespace Katas
+{
+    public class PathFinder_1CanYouReachTheExit
+    {
+        public static void Main(string[] args)
+        {
+            string d = "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       "......\n" +
+                       ".....W\n" +
+                       "......";
+            string a = ".W.\n" +
+                       ".W.\n" +
+                       "...";
+            string b = ".W.\n" +
+                       ".W.\n" +
+                       "W..";
+
+            Console.WriteLine(PathFinder(a));
+        }
+
+        public static bool PathFinder(string maze)
+        {
+            if (maze.Length == 1) return true;
+            var mazeSplit = maze.Split("\n");
+            var mazeIn = new int[mazeSplit.Length][];
+            for (var y = 0; y < mazeSplit.Length; y++)
+            {
+                mazeIn[y] = new int[mazeSplit[y].Length];
+                for (var x = 0; x < mazeSplit[y].Length; x++)
+                {
+                    mazeIn[y][x] = (mazeSplit[y][x] == '.') ? 0 : 1;
+                }
+            }
+
+            mazeIn[0][0] = 2;
+            if (mazeIn[0][1] == 0) mazeIn[0][1] = 2;
+            if (mazeIn[1][0] == 0) mazeIn[1][0] = 2;
+
+            bool test = true;
+            while (test)
+            {
+                test = false;
+                for (var y = 0; y < mazeIn.Length; y++)
+                {
+                    for (var x = 0; x < mazeIn[y].Length; x++)
+                    {
+                        if (mazeIn[y][x] == 2 && x < mazeIn[y].Length - 1 && mazeIn[y][x + 1] == 0)
+                        {
+                            mazeIn[y][x + 1] = 2;
+                            test = true;
+                        }
+
+                        if (mazeIn[y][x] == 2 && y > 0 && mazeIn[y - 1][x] == 0)
+                        {
+                            mazeIn[y - 1][x] = 2;
+                            test = true;
+                        }
+
+                        if (mazeIn[y][x] == 2 && x > 0 && mazeIn[y][x - 1] == 0)
+                        {
+                            mazeIn[y][x - 1] = 2;
+                            test = true;
+                        }
+
+                        if (mazeIn[y][x] == 2 && y < mazeIn.Length - 1 && mazeIn[y + 1][x] == 0)
+                        {
+                            mazeIn[y + 1][x] = 2;
+                            test = true;
+                        }
+
+                        if (mazeIn[mazeIn.Length - 2][mazeIn[0].Length - 1] == 2) return true;
+                        if (mazeIn[mazeIn.Length - 1][mazeIn[0].Length - 2] == 2) return true;
+                    }
+                }
+            }
+
+
+            if (mazeIn[mazeIn.Length - 2][mazeIn[0].Length - 1] == 2) return true;
+            return mazeIn[mazeIn.Length - 1][mazeIn[0].Length - 2] == 2;
+        }
+    }
+}
