0dbf01d9ae9b0ad1a07e76720bcb9cb266476a82
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-01-15 16:08:13 +0100

    next

--- C#/Katas/Katas/By4Skyscrapers.cs
+++ C#/Katas/Katas/By4Skyscrapers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace Katas
@@ -9,18 +10,14 @@ namespace Katas
     {
         public static void Main(string[] args)
         {
-            //todo                             v           v           v           v
-            int[] cluesForTest =    {0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0};
-            // int[] cluesForTest = {0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0};  //todo  <--- good
-            // SolvePuzzle(cluesForTest);
+            var test1 = new HashSet<int> {1, 2, 3};
+            var test2 = new HashSet<int> {4};
+            var test3 = new HashSet<int> {1, 2, 3};
+            var test4 = new HashSet<int> {5, 6};
+            List<HashSet<int>> listTests = new List<HashSet<int>> {test1, test2, test3, test4};
 
 
-            var  test1 = new HashSet<int>{1,2};
-            var  test2 = new HashSet<int>{3, 4};
-            var  test3 = new HashSet<int>{1,2};
-            var  test4 = new HashSet<int>{1,2,3,4};
-            List<HashSet<int>> listTests = new List<HashSet<int>>{test1, test2, test3, test4};
-            var test = listTests
+            var testSet = listTests
                 .Where(s => s.Count != 1)
                 .SelectMany(n => n)
                 .GroupBy(n => n)
@@ -28,6 +25,17 @@ namespace Katas
                 .ToHashSet();
 
 
+            var test = listTests
+                .Where(s => s.Count != 1)
+                .SelectMany(n => n)
+                .GroupBy(n => n)
+                .Where(g => g.Count() == 1)
+                .ToHashSet();
+
+            foreach (var one in test)
+            {
+                Console.WriteLine(one.Key);
+            }
 
             // var unique = new HashSet<int>();
             //
@@ -38,20 +46,27 @@ namespace Katas
             // }
 
 
-
-
-
-
             var answer = new HashSet<int>();
-
-
 
 
             // test.UnionWith(Enumerable.Range(1, 3));
 
+            //todo                           v           v           v           v
+            int[] cluesForTest = {0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0}; //todo  <--- good
+            //todo                                 v                 v                 v                v
+            // int[] cluesForTest = {6, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            // SolvePuzzle(cluesForTest);
+
+            long n = 348597;
+            long[] after = n.ToString().ToCharArray().Reverse().Select(c => long.Parse(c.ToString())).ToArray();
+            foreach (var c in after)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+
+
         }
-
-
 
         // const int Max = 4, Max = 4, MAXNUMBERS = 4;
         // static int countGood = 0;
@@ -64,7 +79,6 @@ namespace Katas
         public static int[][] SolvePuzzle(int[] clues)
         {
             const int Max = 4;
-            var countGood = 0;
             int[] upClues;
             int[] rightClues;
             int[] bottomClues;
@@ -73,34 +87,171 @@ namespace Katas
 
             SetClues(clues, out upClues, out rightClues, out bottomClues, out leftClues);
             MakeEmptyBoard(board, Max);
-            SetUpEdge(board, upClues, ref countGood, Max);
-            SetRightEdge(board, rightClues, ref countGood, upClues, Max);
-            SetBottomEdge(board, bottomClues, ref countGood, rightClues, Max);
-            SetLeftEdge(board, leftClues, ref countGood, bottomClues, upClues, Max);
-            SetEmptySpaces(board, ref countGood, Max);
-            var numberOfFields = Math.Pow(Max, 2);
-            // while (countGood != numberOfFields)
-            // {
-            //     Remove
-            // }
+            SetUpEdge(board, upClues, Max);
+            SetRightEdge(board, rightClues, upClues, Max);
+            SetBottomEdge(board, bottomClues, rightClues, Max);
+            SetLeftEdge(board, leftClues, bottomClues, upClues, Max);
+            FillEmptySpaces(board, Max);
+            // todo RemoveImpossibleDependOnEdges(board, upClues, rightClues, bottomClues, leftClues, Max);
+
+            while (board.SelectMany(s => s).Any(s => s.Count == 1))
+            {
+                FindReadyFieldAndRemoveFromRowAndColumn(board, Max); //todo have to be second 1
+                // FindMaxAndEdgeAndRemoveImpossible(board, upClues, rightClues, bottomClues, leftClues, Max);
+                FindSingleInRowOrColumnAndSet(board, Max); //todo have to be second 2 <--- have to be LAST !!! ..!5.. 6 (1,4,5) 3 (4,2,1) <--- 2
+
+                Console.WriteLine("test");
+            }
+
             Console.WriteLine(board[0]);
 
 
-
-
-
-
-
-
-
             return new int[1][];
-
-
         }
 
-        private static void SetEmptySpaces(
+        private static bool FindMaxAndEdgeAndRemoveImpossibleNextToMax(
+            IReadOnlyList<HashSet<int>[]> board,
+            IReadOnlyList<int> upClues,
+            IReadOnlyList<int> rightClues,
+            IReadOnlyList<int> bottomClues,
+            IReadOnlyList<int> leftClues, int Max)
+        {
+            var change = false;
+            for (var y = 0; y < Max; y++)
+            {
+                //todo if contains MAX then:
+                //todo     only row or column depend on edges and pass edges too
+                if (!board[y].Any(s => s.Contains(Max))) continue;
+                if (upClues[y] == 0 && bottomClues[y] == 0 && leftClues[y] == 0 && rightClues[y] == 0) continue;
+                change = FindMaxAndEdgeAndRemoveImpossibleNextToMaxLogic(board, upClues, bottomClues, leftClues,
+                             rightClues, Max) || change;
+            }
+
+            return change;
+        }
+
+        private static bool FindMaxAndEdgeAndRemoveImpossibleNextToMaxLogic(
+            IReadOnlyList<HashSet<int>[]> board,
+            IReadOnlyList<int> upClues,
+            IReadOnlyList<int> bottomClues,
+            IReadOnlyList<int> leftClues,
+            IReadOnlyList<int> rightClues,
+            int Max)
+        {
+            var change = false;
+            for (var i = 0; i < Max - 2; i++)
+            {
+                for (var y = 0; y < Max; y++)
+                {
+                    for (var x = 0; x < Max; x++)
+                    {
+                        if (board[y][x].Count != 1 || !board[y][x].Contains(Max - i)) continue;
+                        if (upClues[x] != 0 && y > 1)
+                        {
+                            // if(board[y - 1][x].Contains(Max - i))
+                        }
+
+                        if (bottomClues[x] != 0 && y < Max - 2)
+                        {
+                        }
+
+                        if (leftClues[y] != 0 && x > 1)
+                        {
+                        }
+
+                        if (rightClues[y] != 0 && x < Max - 2)
+                        {
+                        }
+                    }
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+
+        private static void FindReadyFieldAndRemoveFromRowAndColumn(IReadOnlyList<HashSet<int>[]> board, int Max)
+        {
+            for (var y = 0; y < Max; y++)
+            {
+                var single = board[y].Where(s => s.Count == 1).ElementAtOrDefault(0)?.ElementAtOrDefault(0);
+                if (single == 0) continue;
+                RemoveImpossible(board[y], single);
+                var columns = new List<HashSet<int>>();
+                for (var x = 0; x < Max; x++)
+                {
+                    columns.Add(board[y][x]);
+                }
+
+                RemoveImpossible(columns.ToArray(), single);
+            }
+        }
+
+        private static void RemoveImpossible(IEnumerable<HashSet<int>> rowOrColumn, int? single)
+        {
+            foreach (var set in rowOrColumn)
+            {
+                if (set.Count > 1 && single != null)
+                    set.Remove(single.Value);
+            }
+
+            // rowOrColumn
+            //         .Where(s => s.Count > 1)
+            //         .Select(s =>
+            //         {
+            //             s.Remove((int) single);
+            //             return s; //todo remove ?
+            //         });
+        }
+
+        private static bool FindSingleInRowOrColumnAndSet(IReadOnlyList<HashSet<int>[]> board, int Max)
+        {
+            for (var y = 0; y < Max; y++)
+            {
+                if (FindAloneInSetsAndRemoveLogic(board[y])) return true;
+            }
+
+            for (var x = 0; x < Max; x++)
+            {
+                var allSets = new List<HashSet<int>>();
+                for (var y = 0; y < Max; y++)
+                {
+                    allSets.Add(board[y][x]);
+                }
+
+                if (FindAloneInSetsAndRemoveLogic(allSets.ToArray())) return true;
+            }
+
+            return false;
+        }
+
+        private static bool FindAloneInSetsAndRemoveLogic(HashSet<int>[] allSets) //todo bug !!!
+        {
+            var single = allSets //todo changed to single from singles !!!
+                .Where(s => s.Count != 1)
+                .SelectMany(n => n)
+                .GroupBy(n => n)
+                .Where(g => g.Count() == 1)
+                .SelectMany(s => s)
+                .FirstOrDefault();
+            if (single == 0) return false;
+            if (allSets.Any(s => s.Count == 1 && s.Contains(single))) return false;
+            foreach (var set in allSets)
+            {
+                if (set.Count == 1 || !set.Contains(single)) continue;
+                set.Clear();
+                // foreach (var single in singles)
+                // {
+                set.Add(single);
+                // }
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void FillEmptySpaces(
             HashSet<int>[][] board,
-            ref int countGood,
             int Max)
         {
             for (var y = 0; y < Max; y++)
@@ -109,10 +260,8 @@ namespace Katas
                 {
                     if (board[y][x].Count != 0) continue;
                     board[y][x] = ReturnOthersCross(board, y, x, Max);
-                    if (board[y][x].Count == 1) countGood++;
                 }
             }
-
         }
 
         private static HashSet<int> ReturnOthersCross(HashSet<int>[][] board, int y, int x, int Max)
@@ -123,10 +272,12 @@ namespace Katas
             {
                 if (board[y][xTemp].Count == 1) answer.Remove(board[y][xTemp].First());
             }
+
             for (var yTemp = 0; yTemp < Max; yTemp++)
             {
                 if (board[yTemp][x].Count == 1) answer.Remove(board[yTemp][x].First());
             }
+
             return answer;
         }
 
@@ -134,7 +285,6 @@ namespace Katas
         private static void SetLeftEdge(
             HashSet<int>[][] board,
             IReadOnlyList<int> leftClues,
-            ref int countGood,
             IReadOnlyList<int> bottomClues,
             IReadOnlyList<int> upClues,
             int Max)
@@ -145,7 +295,6 @@ namespace Katas
                 {
                     if (board[y][0].Count == 0)
                     {
-                        countGood++;
                         board[y][0].Add(Max);
                     }
                     else
@@ -160,27 +309,26 @@ namespace Katas
                         if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(Max - x);
-                        countGood++;
                     }
                 else if (leftClues[y] != 0)
                 {
                     if (y == board.Length - 1)
                     {
-                        if(board[y][0].Count == 1) continue;
-                        if(leftClues[y] <= bottomClues[0]) continue;
+                        if (board[y][0].Count == 1) continue;
+                        if (leftClues[y] <= bottomClues[0]) continue;
                         board[y][0].Clear();
                         board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
                     }
                     else if (y == 0)
                     {
-                        if(board[y][0].Count == 1) continue;
-                        if(leftClues[y] <= upClues[0]) continue;
+                        if (board[y][0].Count == 1) continue;
+                        if (leftClues[y] <= upClues[0]) continue;
                         board[y][0].Clear();
                         board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
                     }
                     else
                     {
-                        if(board[y][0].Count == 1) continue;
+                        if (board[y][0].Count == 1) continue;
                         board[y][0].Clear();
                         board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
                     }
@@ -191,7 +339,6 @@ namespace Katas
         private static void SetBottomEdge(
             HashSet<int>[][] board,
             IReadOnlyList<int> bottomClues,
-            ref int countGood,
             IReadOnlyList<int> rightClues,
             int Max)
         {
@@ -201,7 +348,6 @@ namespace Katas
                 {
                     if (board[Max - 1][x].Count == 0)
                     {
-                        countGood++;
                         board[Max - 1][x].Add(Max);
                     }
                     else
@@ -216,20 +362,19 @@ namespace Katas
                         if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(Max - y);
-                        countGood++;
                     }
                 else if (bottomClues[x] != 0)
                 {
                     if (x == board[Max - 1].Length - 1)
                     {
-                        if(board[Max - 1][x].Count == 1) continue;
-                        if(bottomClues[x] <= rightClues[Max - 1]) continue;
+                        if (board[Max - 1][x].Count == 1) continue;
+                        if (bottomClues[x] <= rightClues[Max - 1]) continue;
                         board[Max - 1][x].Clear();
                         board[Max - 1][x].UnionWith(Enumerable.Range(1, Max - bottomClues[x] + 1));
                     }
                     else
                     {
-                        if(board[Max - 1][x].Count == 1) continue;
+                        if (board[Max - 1][x].Count == 1) continue;
                         board[Max - 1][x].Clear();
                         board[Max - 1][x].UnionWith(Enumerable.Range(1, Max - bottomClues[x] + 1));
                     }
@@ -240,7 +385,6 @@ namespace Katas
         private static void SetRightEdge(
             HashSet<int>[][] board,
             IReadOnlyList<int> rightClues,
-            ref int countGood,
             IReadOnlyList<int> upClues,
             int Max)
         {
@@ -250,7 +394,6 @@ namespace Katas
                 {
                     if (board[y][Max - 1].Count == 0)
                     {
-                        countGood++;
                         board[y][Max - 1].Add(Max);
                     }
                     else
@@ -265,7 +408,6 @@ namespace Katas
                         if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(Max - x);
-                        countGood++;
                     }
                 else if (rightClues[y] != 0)
                 {
@@ -289,20 +431,17 @@ namespace Katas
         private static void SetUpEdge(
             HashSet<int>[][] board,
             IReadOnlyList<int> upClues,
-            ref int countGood,
             int Max)
         {
             for (var x = 0; x < upClues.Count; x++)
             {
                 if (upClues[x] == 1)
                 {
-                    countGood++;
                     board[0][x].Add(Max);
                 }
                 else if (upClues[x] == Max)
                     for (var y = 0; y < Max; y++)
                     {
-                        countGood++;
                         board[y][x].Add(y + 1);
                     }
                 else if (upClues[x] != 0) board[0][x].UnionWith(Enumerable.Range(1, Max - upClues[x] + 1));
@@ -324,9 +463,9 @@ namespace Katas
         private static void SetClues(
             IReadOnlyCollection<int> clues,
             out int[] upClues,
-            out  int[] rightClues,
-            out  int[] bottomClues,
-            out  int[] leftClues)
+            out int[] rightClues,
+            out int[] bottomClues,
+            out int[] leftClues)
         {
             var quarter = clues.Count / 4;
             upClues = clues.Take(quarter).ToArray();
@@ -336,18 +475,15 @@ namespace Katas
         }
 
 
-
-
-
         //
-        //     // todo    find edge 1 or 4 and fill
-        //     // todo    mark rest of the edge, start from biger       on corner set lover list of numbers???
-        //     // todo    mark rest and set if 1
-        //     // todo    check if one left and set
+        //     // tod o    find edge 1 or 4 and fill
+        //     // tod o    mark rest of the edge, start from biger       on corner set lover list of numbers???
+        //     // tod o    mark rest and set if 1
+        //     // tod o    check if one left and set
         //     // todo    while(solve)
-        //     // todo        check if max skyScraper can be placed and put it
-        //     // todo        check if one left and set
-        //     // todo        check if sets excluded 12 and 12 and 123 than there where is 123 set 3
+        //     // tod o        check if max skyScraper can be placed and put it
+        //     // tod o        check if one left and set
+        //     // tod o        check if sets excluded 12 and 12 and 123 than there where is 123 set 3
         //     // todo        if necessary than set example 1 -> 4 (1,2,3)(1,2)(1,2,3) <- 2 than 4 (..)(..) 3
         //     // todo        if necessary than set example 3 -> (12)(12) 3 4 <- 1 than 2 1 3 4
         //     // todo        remove impossible from temp sets  1 -> 4 (123) (123) (12) <- 3  than 4 (123) (13) (12)
@@ -355,11 +491,11 @@ namespace Katas
         //
         //
         //     // todo        check if there are for example row with edge 2 -> (12)(12) 4 3 <- 2 then resolve 2 1 4 3
-        //     // todo        mark rest and if only one then set
+        //     // tod o        mark rest and if only one then set
         //
         //
-        //     // todo        check and if yes set resolve numbers <---
-        //     // todo        check and if yes set excluded
+        //     // tod o        check and if yes set resolve numbers <---
+        //     // tod o        check and if yes set excluded
         //
         //
         //
