af35ed98ad7b1ce12f9c95a4fd7b489f83289d17
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-01-16 18:10:50 +0100

    Fill ready

--- C#/Katas/Katas/By4Skyscrapers.cs
+++ C#/Katas/Katas/By4Skyscrapers.cs
@@ -53,9 +53,9 @@ namespace Katas
 
             //todo                           v           v           v           v
             // int[] cluesForTest = {1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //todo  <--- good
-            // int[] cluesForTest = {0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0}; //todo  <--- good
+            int[] cluesForTest = {0, 0, 1, 2, 0, 2, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0}; //todo  <--- good
             //todo                                 v                 v                 v                v
-            int[] cluesForTest = {1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
+            // int[] cluesForTest = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6};
             SolvePuzzle(cluesForTest);
 
 
@@ -78,7 +78,7 @@ namespace Katas
 
         public static int[][] SolvePuzzle(int[] clues)
         {
-            const int Max = 6;
+            const int Max = 4;
             int[] upClues;
             int[] rightClues;
             int[] bottomClues;
@@ -88,10 +88,13 @@ namespace Katas
             SetClues(clues, out upClues, out rightClues, out bottomClues, out leftClues);
             MakeEmptyBoard(board, Max);
             // SetUpEdge(board, upClues, Max);
-            SetUpEdge2(board, upClues, rightClues, bottomClues, leftClues, Max);
-            SetRightEdge(board, rightClues, upClues, Max);
-            SetBottomEdge(board, bottomClues, rightClues, Max);
-            SetLeftEdge(board, leftClues, bottomClues, upClues, Max);
+            SetUpEdge2(board, upClues, Max);
+            // SetRightEdge(board, rightClues, upClues, Max);
+            SetRightEdge2(board, rightClues, Max);
+            // SetBottomEdge(board, bottomClues, rightClues, Max);
+            SetBottomEdge2(board, bottomClues, Max);
+            // SetLeftEdge(board, leftClues, bottomClues, upClues, Max);
+            SetLeftEdge2(board, leftClues, Max);
             FillEmptySpaces(board, Max);
             // todo RemoveImpossibleFromEdgesToInside(board, upClues, rightClues, bottomClues, leftClues, Max);
 
@@ -110,7 +113,7 @@ namespace Katas
             return new int[1][];
         }
 
-        private static void SetUpEdge2(IReadOnlyList<HashSet<int>[]> board, IReadOnlyList<int> upClues, int[] rightClues, int[] bottomClues, int[] leftClues, int Max)
+        private static void SetUpEdge2(IReadOnlyList<HashSet<int>[]> board, IReadOnlyList<int> upClues, int Max)
         {
             for (var x = 0; x < Max; x++)
             {
@@ -123,6 +126,7 @@ namespace Katas
                 {
                     for (var y = 0; y < Max; y++)
                     {
+                        // if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(y + 1);
                     }
@@ -144,6 +148,129 @@ namespace Katas
                                  board[y][x].Count > Enumerable.Range(1, Max - upClues[x] + 1 + i).Count()))
                         {
                             board[y][x].UnionWith(Enumerable.Range(1, Max - upClues[x] + 1 + i));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetRightEdge2(
+            HashSet<int>[][] board,
+            IReadOnlyList<int> rightClues,
+            int Max)
+        {
+            for (var y = 0; y < rightClues.Count; y++)
+            {
+                if (rightClues[y] == 1)
+                {
+                    board[y][Max - 1].Clear();
+                    board[y][Max - 1].Add(Max);
+                }
+                else if (rightClues[y] == Max)
+                    for (var x = Max - 1; x >= 0; x--)
+                    {
+                        // if (board[y][x].Count == 1) continue;
+                        board[y][x].Clear();
+                        board[y][x].Add(Max - x);
+                    }
+                else if (rightClues[y] != 0)
+                {
+                    for (int x = Max - 1, i = 0; x >= 0; x--, i++)
+                    {
+                        if (i == 0 &&
+                            (board[y][x].Count == 0 ||
+                             board[y][x].Count > Enumerable.Range(1, Max - rightClues[y] + 1).Count())
+                        )
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1));
+                        }
+                        else if(Max - rightClues[y] + 1 + i < Max &&
+                                (board[y][x].Count == 0 ||
+                                 board[y][x].Count > Enumerable.Range(1, Max - rightClues[y] + 1 + i).Count()))
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1 + i));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetBottomEdge2(
+            HashSet<int>[][] board,
+            IReadOnlyList<int> bottomClues,
+            int Max)
+        {
+            for (var x = Max - 1; x >= 0; x--)
+            {
+                if (bottomClues[x] == 1)
+                {
+                    board[Max - 1][x].Clear();
+                    board[Max - 1][x].Add(Max);
+                }
+                else if (bottomClues[x] == Max)
+                    for (var y = Max - 1; y >= 0; y--)
+                    {
+                        // if (board[y][x].Count == 1) continue;
+                        board[y][x].Clear();
+                        board[y][x].Add(Max - y);
+                    }
+                else if (bottomClues[x] != 0)
+                {
+                    for (int y = Max - 1, i = 0; y >= 0; y--, i++)
+                    {
+                        if (i == 0 &&
+                            (board[y][x].Count == 0 ||
+                             board[y][x].Count > Enumerable.Range(1, Max - bottomClues[x] + 1).Count())
+                        )
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - bottomClues[x] + 1));
+                        }
+                        else if(Max - bottomClues[x] + 1 + i < Max &&
+                                (board[y][x].Count == 0 ||
+                                 board[y][x].Count > Enumerable.Range(1, Max - bottomClues[x] + 1 + i).Count()))
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - bottomClues[x] + 1 + i));
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetLeftEdge2(
+            HashSet<int>[][] board,
+            IReadOnlyList<int> leftClues,
+            int Max)
+        {
+            for (var y = 0; y < leftClues.Count; y++)
+            {
+                if (leftClues[y] == 1)
+                {
+                    board[y][0].Clear();
+                    board[y][0].Add(Max);
+                }
+                else if (leftClues[y] == Max)
+                    for (var x = 0; x < Max; x++)
+                    {
+                        // if (board[y][x].Count == 1) continue;
+                        board[y][x].Clear();
+                        board[y][x].Add(x + 1);
+                    }
+                else if (leftClues[y] != 0)
+                {
+                    for (int x = 0, i = 0; x < Max - 1; x++, i++)
+                    {
+                        if (i == 0 &&
+                            (board[y][x].Count == 0 ||
+                             board[y][x].Count > Enumerable.Range(1, Max - leftClues[y] + 1).Count())
+                        )
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
+                        }
+                        else if(Max - leftClues[y] + 1 + i < Max &&
+                                (board[y][x].Count == 0 ||
+                                 board[y][x].Count > Enumerable.Range(1, Max - leftClues[y] + 1 + i).Count()))
+                        {
+                            board[y][x].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1 + i));
                         }
                     }
                 }
@@ -323,55 +450,67 @@ namespace Katas
         }
 
 
-        private static void SetLeftEdge(
+        private static void SetUpEdge(
             HashSet<int>[][] board,
-            IReadOnlyList<int> leftClues,
-            IReadOnlyList<int> bottomClues,
             IReadOnlyList<int> upClues,
             int Max)
         {
-            for (var y = 0; y < leftClues.Count; y++)
+            for (var x = 0; x < upClues.Count; x++)
             {
-                if (leftClues[y] == 1)
+                if (upClues[x] == 1)
                 {
-                    if (board[y][0].Count == 0)
+                    board[0][x].Add(Max);
+                }
+                else if (upClues[x] == Max)
+                    for (var y = 0; y < Max; y++)
                     {
-                        board[y][0].Add(Max);
+                        board[y][x].Add(y + 1);
+                    }
+                else if (upClues[x] != 0) board[0][x].UnionWith(Enumerable.Range(1, Max - upClues[x] + 1));
+            }
+        }
+
+        private static void SetRightEdge(
+            HashSet<int>[][] board,
+            IReadOnlyList<int> rightClues,
+            IReadOnlyList<int> upClues,
+            int Max)
+        {
+            for (var y = 0; y < rightClues.Count; y++)
+            {
+                if (rightClues[y] == 1)
+                {
+                    if (board[y][Max - 1].Count == 0)
+                    {
+                        board[y][Max - 1].Add(Max);
                     }
                     else
                     {
-                        board[y][0].Clear();
-                        board[y][0].Add(Max);
+                        board[y][Max - 1].Clear();
+                        board[y][Max - 1].Add(Max);
                     }
                 }
-                else if (leftClues[y] == Max)
-                    for (var x = 0; x < Max; x++)
+                else if (rightClues[y] == Max)
+                    for (var x = Max - 1; x >= 0; x--)
                     {
                         if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(Max - x);
                     }
-                else if (leftClues[y] != 0)
+                else if (rightClues[y] != 0)
                 {
-                    if (y == board.Length - 1)
+                    if (y == 0)
                     {
-                        if (board[y][0].Count == 1) continue;
-                        if (leftClues[y] <= bottomClues[0]) continue;
-                        board[y][0].Clear();
-                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
-                    }
-                    else if (y == 0)
-                    {
-                        if (board[y][0].Count == 1) continue;
-                        if (leftClues[y] <= upClues[0]) continue;
-                        board[y][0].Clear();
-                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
+                        if (board[y][Max - 1].Count == 1) continue;
+                        if (rightClues[y] <= upClues[Max - 1]) continue;
+                        board[y][Max - 1].Clear();
+                        board[y][Max - 1].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1));
                     }
                     else
                     {
-                        if (board[y][0].Count == 1) continue;
-                        board[y][0].Clear();
-                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
+                        if (board[y][Max - 1].Count == 1) continue;
+                        board[y][Max - 1].Clear();
+                        board[y][Max - 1].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1));
                     }
                 }
             }
@@ -423,69 +562,57 @@ namespace Katas
             }
         }
 
-        private static void SetRightEdge(
+        private static void SetLeftEdge(
             HashSet<int>[][] board,
-            IReadOnlyList<int> rightClues,
+            IReadOnlyList<int> leftClues,
+            IReadOnlyList<int> bottomClues,
             IReadOnlyList<int> upClues,
             int Max)
         {
-            for (var y = 0; y < rightClues.Count; y++)
+            for (var y = 0; y < leftClues.Count; y++)
             {
-                if (rightClues[y] == 1)
+                if (leftClues[y] == 1)
                 {
-                    if (board[y][Max - 1].Count == 0)
+                    if (board[y][0].Count == 0)
                     {
-                        board[y][Max - 1].Add(Max);
+                        board[y][0].Add(Max);
                     }
                     else
                     {
-                        board[y][Max - 1].Clear();
-                        board[y][Max - 1].Add(Max);
+                        board[y][0].Clear();
+                        board[y][0].Add(Max);
                     }
                 }
-                else if (rightClues[y] == Max)
-                    for (var x = Max - 1; x >= 0; x--)
+                else if (leftClues[y] == Max)
+                    for (var x = 0; x < Max; x++)
                     {
                         if (board[y][x].Count == 1) continue;
                         board[y][x].Clear();
                         board[y][x].Add(Max - x);
                     }
-                else if (rightClues[y] != 0)
+                else if (leftClues[y] != 0)
                 {
-                    if (y == 0)
+                    if (y == board.Length - 1)
                     {
-                        if (board[y][Max - 1].Count == 1) continue;
-                        if (rightClues[y] <= upClues[Max - 1]) continue;
-                        board[y][Max - 1].Clear();
-                        board[y][Max - 1].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1));
+                        if (board[y][0].Count == 1) continue;
+                        if (leftClues[y] <= bottomClues[0]) continue;
+                        board[y][0].Clear();
+                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
+                    }
+                    else if (y == 0)
+                    {
+                        if (board[y][0].Count == 1) continue;
+                        if (leftClues[y] <= upClues[0]) continue;
+                        board[y][0].Clear();
+                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
                     }
                     else
                     {
-                        if (board[y][Max - 1].Count == 1) continue;
-                        board[y][Max - 1].Clear();
-                        board[y][Max - 1].UnionWith(Enumerable.Range(1, Max - rightClues[y] + 1));
+                        if (board[y][0].Count == 1) continue;
+                        board[y][0].Clear();
+                        board[y][0].UnionWith(Enumerable.Range(1, Max - leftClues[y] + 1));
                     }
                 }
-            }
-        }
-
-        private static void SetUpEdge(
-            HashSet<int>[][] board,
-            IReadOnlyList<int> upClues,
-            int Max)
-        {
-            for (var x = 0; x < upClues.Count; x++)
-            {
-                if (upClues[x] == 1)
-                {
-                    board[0][x].Add(Max);
-                }
-                else if (upClues[x] == Max)
-                    for (var y = 0; y < Max; y++)
-                    {
-                        board[y][x].Add(y + 1);
-                    }
-                else if (upClues[x] != 0) board[0][x].UnionWith(Enumerable.Range(1, Max - upClues[x] + 1));
             }
         }
 
