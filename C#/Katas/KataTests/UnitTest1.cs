2b98e38d015a0d63e8e8d09e98a474e7c4793d42
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-02-26 22:33:24 +0100

    next

--- C#/Katas/KataTests/UnitTest1.cs
+++ C#/Katas/KataTests/UnitTest1.cs
@@ -1,25 +1,43 @@
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using Katas;
+using NUnit.Framework;
 
-[TestFixture]
-public class Tests
+public class SolutionTest
 {
     [Test]
-    public static void FixedTest_aaaa()
+    public void EncryptExampleTests()
     {
-        Dictionary<char, int> d = new Dictionary<char, int>();
-        d.Add('a', 4);
-        Assert.AreEqual(d, kyu6CountCharactersInYourString.Count("aaaa"));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", 0));
+        Assert.AreEqual("hsi  etTi sats!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", 1));
+        Assert.AreEqual("s eT ashi tist!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", 2));
+        Assert.AreEqual(" Tah itse sits!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", 3));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", 4));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This is a test!", -1));
+        Assert.AreEqual("hskt svr neetn!Ti aai eyitrsig", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("This kata is very interesting!", 1));
     }
-
+  
     [Test]
-    public static void FixedTest_aabb()
+    public void DecryptExampleTests()
     {
-        Dictionary<char, int> d = new Dictionary<char, int>();
-        d.Add('a', 2);
-        d.Add('b', 2);
-        Assert.AreEqual(d, kyu6CountCharactersInYourString.Count("aabb"));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("This is a test!", 0));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("hsi  etTi sats!", 1));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("s eT ashi tist!", 2));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt(" Tah itse sits!", 3));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("This is a test!", 4));
+        Assert.AreEqual("This is a test!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("This is a test!", -1));
+        Assert.AreEqual("This kata is very interesting!", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+    }
+  
+    [Test]
+    public void EmptyTests()
+    {
+        Assert.AreEqual("", kyu6SimpleEncryption_1_AlternatingSplit.Encrypt("", 0));
+        Assert.AreEqual("", kyu6SimpleEncryption_1_AlternatingSplit.Decrypt("", 0));
+    }
+  
+    [Test]
+    public void NullTests()
+    {
+        Assert.AreEqual(null, kyu6SimpleEncryption_1_AlternatingSplit.Encrypt(null, 0));
+        Assert.AreEqual(null, kyu6SimpleEncryption_1_AlternatingSplit.Decrypt(null, 0));
     }
 }
---
+++ C#/Katas/Katas/kyu6SimpleEncryption#1-AlternatingSplit.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Text;
+
+namespace Katas
+{
+    public class kyu6SimpleEncryption_1_AlternatingSplit
+    {
+        public static void Main(string[] args)
+        {
+            string test = "abcd!";
+
+            Console.WriteLine(Encrypt(test, 1));
+            Console.WriteLine(Decrypt("bdac!", 1));
+            // Console.WriteLine(Encrypt("This is a test!", 1).Equals("hsi  etTi sats!"));
+            // Console.WriteLine(Decrypt("hsi  etTi sats!", 1));
+            // Console.WriteLine(Encrypt("s eT ashi tist!", 2));
+        }
+
+        public static string Encrypt(string text, int n)
+        {
+            if(n < 1) return text;
+            if(text == null) return null;
+            var answer = new StringBuilder(text);
+            while (n-- > 0)
+            {
+                var partOne = new StringBuilder();
+                var partTwo = new StringBuilder();
+                for (var i = 1; i < text.Length; i += 2)
+                {
+                    partOne.Append(answer[i]);
+                }
+                for (var i = 0; i < text.Length; i += 2)
+                {
+                    partTwo.Append(answer[i]);
+                }
+                answer = new StringBuilder(partOne.ToString() + partTwo);
+            }
+            return answer.ToString();
+        }
+
+        public static string Decrypt(string encryptedText, int n)
+        {
+            if(n < 1) return encryptedText;
+            if(encryptedText == null) return null;
+            StringBuilder answer = new StringBuilder();
+            while (n-- > 0)
+            {
+                answer = new StringBuilder(encryptedText.Length);
+                for (int x = 0, i = 0, j = encryptedText.Length / 2; x < encryptedText.Length; i++, j++, x++)
+                {
+                    answer.Insert(x++, encryptedText[j]);
+                    if(x >= encryptedText.Length) break;
+                    answer.Insert(x, encryptedText[i]);
+                }
+                encryptedText = answer.ToString();
+            }
+            return answer.ToString();
+        }
+    }
+
+}
