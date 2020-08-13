92840aaf7e83abdac86f7cadf1f4ffe088c67f7c
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-08-13 19:47:07 +0200

    next

---
+++ Python/kyu6RankVector.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def ranks(a):
+	print(a)
+	answer = a.copy()
+	rank = 1
+	for k, v in dict(Counter(list(sorted(a, reverse=True)))).items():
+		for i in [i for i, e in enumerate(a) if e == k]:
+			answer[i] = rank
+		rank += v
+	return answer
+	
+
--- Python/test.py
+++ Python/test.py
@@ -5,7 +5,9 @@ from kyu6CatKataPart1 import peaceful_yard
 from kyu6CompareVersions import compare_versions
 from kyu6PeteTheBakerPpart2 import get_missing_ingredients
 from kyu6PrimeFactorization import PrimeFactorizer
+from kyu6RankVector import ranks
 from kyu6SortSentencePseudoAlphabetically import pseudo_sort
 
-test.assert_equals(PrimeFactorizer(13).factor, {13: 1})
-test.assert_equals(PrimeFactorizer(24).factor, {2: 3, 3: 1})
+Test.assert_equals(ranks([]), [])
+Test.assert_equals(ranks([2]), [1])
+Test.assert_equals(ranks([2,2]), [1,1])
