1c124de0772cfde96cdf40414d47b27d6f5026e4
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-08-13 19:01:58 +0200

    next

---
+++ Python/kyu6PrimeFactorization.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+
+class PrimeFactorizer:
+	def __init__(self, number):
+		answer = []
+		n = 2
+		while n <= number:
+			for n in PrimeFactorizer.gen_primes():
+				if number % n == 0:
+					number //= n
+					answer.append(n)
+					break
+		self.factor = dict(Counter(answer))
+
+
+	# Sieve of Eratosthenes
+	# Code by David Eppstein, UC Irvine, 28 Feb 2002
+	# http://code.activestate.com/recipes/117119/
+	@staticmethod
+	def gen_primes():
+		D = {}
+		q = 2
+		while True:
+			if q not in D:
+				yield q
+				D[q * q] = [q]
+			else:
+				for p in D[q]:
+					D.setdefault(p + q, []).append(p)
+				del D[q]
+			q += 1
--- Python/test.py
+++ Python/test.py
@@ -4,13 +4,8 @@ import codewars_test as test
 from kyu6CatKataPart1 import peaceful_yard
 from kyu6CompareVersions import compare_versions
 from kyu6PeteTheBakerPpart2 import get_missing_ingredients
+from kyu6PrimeFactorization import PrimeFactorizer
 from kyu6SortSentencePseudoAlphabetically import pseudo_sort
 
-Test.assert_equals(compare_versions("11", "10"), True, 'Testing versions without subversion')
-Test.assert_equals(compare_versions("11", "11"), True, 'Test equal versions')
-Test.assert_equals(compare_versions("10.4.6", "10.4"), True, 'Adding a subversion should make this version "larger"')
-
-Test.assert_equals(compare_versions("10.4", "10.4.8"), False, 'Adding a subversion should make this version "larger"')
-Test.assert_equals(compare_versions("10.4", "11"), False, 'Subversion precedence is smaller than Version')
-Test.assert_equals(compare_versions("10.4", "10.10"), False, 'Version value is not the same as its decimal value')
-Test.assert_equals(compare_versions("10.4.9", "10.5"), False, 'Adding subversion does not make it larger than a greater version')
+test.assert_equals(PrimeFactorizer(13).factor, {13: 1})
+test.assert_equals(PrimeFactorizer(24).factor, {2: 3, 3: 1})
