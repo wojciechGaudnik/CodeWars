b597c29c3832930f6b89dc5c91c1b89bd3c078e2
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-09-23 20:12:29 +0200

    next

---
+++ Groovy/src/simple_fun63_shape_area/kyu7/KataTest.groovy
@@ -0,0 +1,13 @@
+package simple_fun63_shape_area.kyu7
+
+import org.junit.Test
+
+class KataTest {
+    @Test
+    void "Example Tests"() {
+        assert Kata.shapeArea(2) == 5
+        assert Kata.shapeArea(3) == 13
+        assert Kata.shapeArea(1) == 1
+        assert Kata.shapeArea(5) == 41
+    }
+}
