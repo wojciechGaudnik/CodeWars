aea5bcdd21993d4bd206bb8b24ff445fd6fe313e
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-10-21 21:32:00 +0200

    next

---
+++ Java/src/sort_the_gift_code/kyu7/GiftSorter.java
@@ -0,0 +1,13 @@
+package sort_the_gift_code.kyu7;
+
+import java.util.Arrays;
+
+public class GiftSorter {
+
+	public String sortGiftCode(String code){
+		var array = code.toCharArray();
+		Arrays.sort(array);
+		return String.valueOf(array);
+	}
+}
+   
---
+++ Java/src/sort_the_gift_code/kyu7/GiftSorterTest.java
@@ -0,0 +1,20 @@
+//package sort_the_gift_code.kyu7;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//class GiftSorterTest {
+//	@Test
+//	public void Tests1() throws Exception {
+//		GiftSorter gs = new GiftSorter();
+//		assertEquals("sort fedcba", "abcdef", gs.sortGiftCode("fedcba"));
+//	}
+//
+//	@Test
+//	public void Tests2() throws Exception {
+//		GiftSorter gs = new GiftSorter();
+//		assertEquals("reverse alphabet", "abcdefghijklmnopqrstuvwxyz", gs.sortGiftCode("zyxwvutsrqponmlkjihgfedcba"));
+//	}
+//
+//}
