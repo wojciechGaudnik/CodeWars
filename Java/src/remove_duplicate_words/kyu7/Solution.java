010b211cc27f393e58a908d76a2cb5a27afb62e3
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-10-22 21:16:45 +0200

    next

---
+++ Java/src/remove_duplicate_words/kyu7/Solution.java
@@ -0,0 +1,15 @@
+package remove_duplicate_words.kyu7;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println(removeDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+	}
+
+	public static String removeDuplicateWords(String s){
+		return Arrays.stream(s.split(" ")).distinct().collect(Collectors.joining(" "));
+	}
+}
+   
