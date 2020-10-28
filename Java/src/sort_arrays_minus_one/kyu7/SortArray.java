974210adcdcd7e65ce33b28a685dec5161753297
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-10-28 22:06:30 +0100

    next

---
+++ Java/src/sort_arrays_minus_one/kyu7/SortArray.java
@@ -0,0 +1,52 @@
+package sort_arrays_minus_one.kyu7;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static int[] sortArray(int[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static String[] sortArray(String[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static Integer[] sortArray(Integer[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static long[] sortArray(long[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static Long[] sortArray(Long[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static double[] sortArray(double[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static Double[] sortArray(Double[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static float[] sortArray(float[] names){
+		Arrays.sort(names);
+		return names;
+	}
+
+	public static Float[] sortArray(Float[] names){
+		Arrays.sort(names);
+		return names;
+	}
+}
+   
