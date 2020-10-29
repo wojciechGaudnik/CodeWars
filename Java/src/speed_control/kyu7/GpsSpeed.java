e6decbf9115b9487d4119a3eeb8189fdf37d5d21
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-10-29 20:54:38 +0100

    next

---
+++ Java/src/speed_control/kyu7/GpsSpeed.java
@@ -0,0 +1,16 @@
+package speed_control.kyu7;
+
+public class GpsSpeed {
+
+	public static int gps(int s, double[] x) {
+		if (x.length <= 1) return 0;
+		double maxDist = 0;
+		for (var i = 1; i < x.length; i++) {
+			if (x[i] - x[i - 1] > maxDist) {
+				maxDist = x[i] - x[i - 1];
+			}
+		}
+		return (int)Math.floor((3600 * (maxDist)) / s);
+	}
+}
+   
