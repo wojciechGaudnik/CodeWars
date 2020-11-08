21b2c74441b65a9c6d7eb079bbca527bec835567
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-11-08 07:55:11 +0100

    next

---
+++ Java/src/averages_of_numbers/kyu7/Kata.java
@@ -0,0 +1,20 @@
+package averages_of_numbers.kyu7;
+
+public class Kata {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static double[] averages(int[] numbers){
+		if (numbers == null || numbers.length <= 1) {
+			return new double[0];
+		}
+		var answer = new double[numbers.length - 1];
+		for (var i = 0; i < numbers.length - 1; i++) {
+			answer[i] = (numbers[i] + numbers[i + 1]) / 2.0;
+		}
+		return answer;
+	}
+}
+   
