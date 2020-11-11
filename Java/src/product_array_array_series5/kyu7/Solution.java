fab96a04ecd4533eaca8126193fa8bf0cf362f73
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-11-11 16:47:35 +0100

    next

---
+++ Java/src/product_array_array_series5/kyu7/Solution.java
@@ -0,0 +1,19 @@
+package product_array_array_series5.kyu7;
+
+import java.math.BigInteger;
+
+public class Solution {
+
+	public static long[] productArray(int[] numbers){
+		BigInteger prodAll = BigInteger.ONE;
+		for (int number : numbers) {
+			prodAll = prodAll.multiply(BigInteger.valueOf(number));
+		}
+		var answer = new long[numbers.length];
+		for (var i = 0; i < numbers.length; i++) {
+			answer[i] = prodAll.divide(BigInteger.valueOf(numbers[i])).longValue();
+		}
+		return answer;
+	}
+}
+   
