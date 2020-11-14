a9ee0fc034f47b61b1b9c9f5b45ab4662a3475df
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-11-14 17:12:38 +0100

    next

---
+++ Java/src/array_leaders_array_series_3/kyu7/Solution.java
@@ -0,0 +1,21 @@
+package array_leaders_array_series_3.kyu7;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+
+	public static List arrayLeaders(int[] numbers){
+		var sumAll = Arrays.stream(numbers).sum();
+		var answer = new LinkedList<>();
+		for (var one : numbers) {
+			sumAll -= one;
+			if (one > sumAll) {
+				answer.add(one);
+			}
+		}
+		return answer;
+	}
+}
+   
