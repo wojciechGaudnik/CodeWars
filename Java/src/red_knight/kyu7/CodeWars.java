91818116ecb675d59bfdd0ab1f88bcebc678f198
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-12-12 10:49:10 +0100

    next

--- Java/src/red_knight/kyu7/CodeWars.java
+++ Java/src/red_knight/kyu7/CodeWars.java
@@ -2,31 +2,12 @@ package red_knight.kyu7;
 
 public class CodeWars {
 
-	public static void main(String[] args) {
-		var test = redKnight(0, 8);
-		System.out.println(test.color);
-		System.out.println(test.distance);
-
-		test = redKnight(0, 7);
-		System.out.println(test.color);
-		System.out.println(test.distance);
-
-		test = redKnight(1, 6);
-		System.out.println(test.color);
-		System.out.println(test.distance);
-
-		test = redKnight(1, 5);
-		System.out.println(test.color);
-		System.out.println(test.distance);
-	}
-
 	public static PawnDistance redKnight(int knight, long pawn) {
 		if (knight == 0 && pawn % 2 == 0) return new PawnDistance("White", pawn * 2);
 		if (knight == 0 && pawn % 2 == 1) return new PawnDistance("Black", pawn * 2);
 		if (knight == 1 && pawn % 2 == 0) return new PawnDistance("Black", pawn * 2);
 		return new PawnDistance("White", pawn * 2);
 	}
-
 
 	static class PawnDistance {
 		private String color;
@@ -36,5 +17,4 @@ public class CodeWars {
 			distance = d;
 		}
 	}
-
 }
---
+++ Java/src/thinking_testing_uniq_or_not_uniq/kyu7/Kata.java
@@ -0,0 +1,15 @@
+package thinking_testing_uniq_or_not_uniq.kyu7;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Kata {
+
+	public static int[] testit(int[] a, int[] b){
+		var listA = Arrays.stream(a).distinct().boxed().collect(Collectors.toList());
+		var listB = Arrays.stream(b).distinct().boxed().collect(Collectors.toList());
+		listA.addAll(listB);
+		return listA.stream().sorted().mapToInt(Integer::intValue).toArray();
+	}
+
+}
