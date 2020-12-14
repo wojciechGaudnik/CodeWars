0fd2c02b9a6d404f80681dbbcc40e86dc4c74654
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-12-14 17:53:07 +0100

    next

---
+++ Java/src/palindrome_chain_length/kyu7/Palindromes.java
@@ -0,0 +1,22 @@
+package palindrome_chain_length.kyu7;
+
+public class Palindromes {
+
+	public static void main(String[] args) {
+		System.out.println(palindromeChainLength(89));
+	}
+
+	public static int palindromeChainLength (long n) {
+		var answer = 0;
+		while (!isPalindrome(n)) {
+			n += Long.parseLong(new StringBuilder(String.valueOf(n)).reverse().toString());
+			answer++;
+		}
+		return answer;
+	}
+
+	private static boolean isPalindrome(long m) {
+		return String.valueOf(m).equals(new StringBuilder(String.valueOf(m)).reverse().toString());
+	}
+
+}
