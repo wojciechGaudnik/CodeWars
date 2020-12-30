e0a9b24e0c9449250c8d1fff2a46084ad63a7fb5
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-12-30 16:08:45 +0100

    next

---
+++ Java/src/complete_the_pattern_2/Kyu7/CompleteThePattern.java
@@ -0,0 +1,27 @@
+package complete_the_pattern_2.Kyu7;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+public class CompleteThePattern {
+
+	public static void main(String[] args) {
+		System.out.println(pattern(5));
+	}
+
+	public static String pattern (int n) {
+		if (n <= 0) {
+			return "";
+		}
+		var answer = new StringBuilder();
+		var i = n - 1;
+		while (i >= 0) {
+			answer.append(IntStream.range(n - i--, n + 1)
+					.boxed()
+					.sorted(Collections.reverseOrder())
+					.map(String::valueOf).reduce((a, b) -> a + b).orElseGet(() -> ""))
+			.append("\n");
+		}
+		return answer.substring(0, answer.length() - 1);
+	}
+}
--- Java/src/name_array_capping/kyu7/Kata.java
+++ Java/src/name_array_capping/kyu7/Kata.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Kata {
 
 	public static String[] capMe(String[] strings){
-		return Arrays.stream(strings).map(s -> Character.toTitleCase(s.charAt(0)) + s.substring(1).toLowerCase()).toArray(String[]::new);
+		return Arrays.stream(strings)
+				.map(s -> Character.toTitleCase(s.charAt(0)) + s.substring(1).toLowerCase())
+				.toArray(String[]::new);
 	}
 }
