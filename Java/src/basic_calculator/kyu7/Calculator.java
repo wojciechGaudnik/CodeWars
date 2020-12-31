2feef6ae63ed73f006862e551aad00156830dfd9
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2020-12-31 10:48:10 +0100

    next

---
+++ Java/src/basic_calculator/kyu7/Calculator.java
@@ -0,0 +1,18 @@
+package basic_calculator.kyu7;
+
+public class Calculator {
+
+	public static Double calculate(final double numberOne, final String operation, final double numberTwo){
+		switch (operation) {
+			case "+":
+				return numberOne + numberTwo;
+			case "-":
+				return numberOne - numberTwo;
+			case "*":
+				return (numberOne == 0 || numberTwo == 0) ? 0: numberOne * numberTwo;
+			case "/":
+				return (numberTwo != 0) ?numberOne / numberTwo: null;
+		}
+		return null;
+	}
+}
