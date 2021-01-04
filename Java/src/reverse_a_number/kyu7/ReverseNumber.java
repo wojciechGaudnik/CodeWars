4dd6188636f052e3bf4aa6a546e6c80712ab3cb1
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2021-01-04 14:53:04 +0100

    next

---
+++ Java/src/reverse_a_number/kyu7/ReverseNumber.java
@@ -0,0 +1,10 @@
+package reverse_a_number.kyu7;
+
+public class ReverseNumber {
+
+	public static int reverse(int number) {
+		var minus = number < 0;
+		var reversedNumber = Integer.valueOf(new StringBuilder(String.valueOf(Math.abs(number))).reverse().toString());
+		return (minus)? -reversedNumber: reversedNumber;
+	}
+}
