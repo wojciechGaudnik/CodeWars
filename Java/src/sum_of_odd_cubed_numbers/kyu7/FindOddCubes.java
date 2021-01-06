bd2e3ec33398e4b633f9825cf60c1fc81707a497
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2021-01-06 16:04:43 +0100

    next

---
+++ Java/src/sum_of_odd_cubed_numbers/kyu7/FindOddCubes.java
@@ -0,0 +1,11 @@
+package sum_of_odd_cubed_numbers.kyu7;
+
+import java.util.Arrays;
+
+public class FindOddCubes {
+
+	public static int cubeOdd(int arr[]) {
+		return Arrays.stream(arr).filter(e -> e % 2 == 1 || e % 2 == -1).map(e -> (int) Math.pow(e, 3)).sum();
+	}
+
+}
