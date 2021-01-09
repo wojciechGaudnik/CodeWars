8ef4889f3e92d56b21f8d8b474d39754f27d2f6f
wojciechgaudnik <wojciech.gaudnik@gmail.com>
2021-01-09 08:12:54 +0100

    next

---
+++ Java/src/shorter_concat_reverse_longer/kyu7/ReverseLonger.java
@@ -0,0 +1,10 @@
+package shorter_concat_reverse_longer.kyu7;
+
+public class ReverseLonger {
+
+	public static String shorterReverseLonger(String a, String b) {
+		var longer = new StringBuffer((a.length() >= b.length()) ? a: b).reverse().toString();
+		var shorter = (a.length() >= b.length()) ? b : a;
+		return shorter + longer + shorter;
+	}
+}
---
+++ Java/src/shorter_concat_reverse_longer/kyu7/ReverseLongerTest.java
@@ -0,0 +1,16 @@
+package shorter_concat_reverse_longer.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReverseLongerTest {
+
+	@Test
+	public void static1() {
+		assertEquals("abcdetsrifabcde",ReverseLonger.shorterReverseLonger("first", "abcde"));
+		assertEquals("bauollehbau",ReverseLonger.shorterReverseLonger("hello", "bau"));
+		assertEquals("fghiedcbafghi",ReverseLonger.shorterReverseLonger("abcde", "fghi"));
+	}
+
+}
