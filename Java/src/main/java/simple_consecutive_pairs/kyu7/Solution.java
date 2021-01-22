48b8230220f5bf0124c4daa4914433de01b68556
bq666 <wojciech.gaudnik@gmail.com>
2021-01-22 20:34:09 +0100

    next

---
+++ Java/src/main/java/simple_consecutive_pairs/kyu7/Solution.java
@@ -0,0 +1,13 @@
+package simple_consecutive_pairs.kyu7;
+
+public class Solution {
+	public static int solve(int [] arr){
+		var answer = 0;
+		for (var i = 1; i < arr.length; i += 2) {
+			if (Math.abs(arr[i - 1] - arr[i]) == 1) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
