6cd290ba31803bf3440b945a0d4456bc32e7eca6
bq666 <wojciech.gaudnik@gmail.com>
2021-02-16 10:37:09 +0100

    next

---
+++ Java/src/main/java/is_n_divisigle_by/kyu7/Divisible.java
@@ -0,0 +1,2 @@
+package is_n_divisigle_by.kyu7;public class Divisible {
+}
---
+++ Java/src/test/java/is_n_divisigle_by/kyu7/DivisibleTest.java
@@ -0,0 +1,7 @@
+package is_n_divisigle_by.kyu7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisibleTest {
+
+}
