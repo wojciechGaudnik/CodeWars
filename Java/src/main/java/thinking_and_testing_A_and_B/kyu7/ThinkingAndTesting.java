4204c9e2a07c2eb6cc67d76eee7871080315e368
bq666 <wojciech.gaudnik@gmail.com>
2021-02-21 18:40:36 +0100

    next

---
+++ Java/src/main/java/thinking_and_testing_A_and_B/kyu7/ThinkingAndTesting.java
@@ -0,0 +1,11 @@
+package thinking_and_testing_A_and_B.kyu7;
+
+public class ThinkingAndTesting {
+	public static void main(String[] args) {
+
+	}
+
+	public static int testAB(int a, int b) {
+		return a | b;
+	}
+}
---
+++ Java/src/test/java/thinking_and_testing_A_and_B/kyu7/ThinkingAndTestingTest.java
@@ -0,0 +1,22 @@
+package thinking_and_testing_A_and_B.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ThinkingAndTestingTest {
+	@Test
+	public void exampleSumTests() {
+		// a + b?
+		assertEquals(1, ThinkingAndTesting.testAB(0, 1));
+		assertEquals(3, ThinkingAndTesting.testAB(1, 2));
+		assertEquals(30, ThinkingAndTesting.testAB(10, 20));
+	}
+
+	@Test
+	public void exampleProductTests() {
+		// a * b?
+		assertEquals(1, ThinkingAndTesting.testAB(1, 1));
+		assertEquals(3, ThinkingAndTesting.testAB(1, 3));
+	}
+}
