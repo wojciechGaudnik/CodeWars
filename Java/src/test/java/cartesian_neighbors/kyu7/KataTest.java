4b46bd70e0130e206bf45414f167415fa34034e8
bq666 <wojciech.gaudnik@gmail.com>
2021-03-10 19:30:22 +0100

    next

---
+++ Java/src/test/java/cartesian_neighbors/kyu7/KataTest.java
@@ -0,0 +1,10 @@
+package cartesian_neighbors.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+class KataTest {
+	@Test
+	public void ExampleTest() {
+//		assertArrayEquals("", Inspection.sortedList(Kata.cartesianNeighbor(2, 2)), new int[][]{{1,1},{1,2},{1,3},{2,1},{2,3},{3,1},{3,2},{3,3}} );
+	}
+}
