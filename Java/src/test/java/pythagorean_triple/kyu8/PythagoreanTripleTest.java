6360705e76fd3d6c2b4010899dfdfebe1f5d4745
bq666 <wojciech.gaudnik@gmail.com>
2021-05-06 16:40:00 +0200

    next

---
+++ Java/src/test/java/pythagorean_triple/kyu8/PythagoreanTripleTest.java
@@ -0,0 +1,23 @@
+package pythagorean_triple.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PythagoreanTripleTest {
+	static int[] p1={3,4,5};
+	static int[] p2={3,5,7};
+
+	PythagoreanTriple pT = new PythagoreanTriple();
+
+	@Test
+	public void validTriple() {
+		assertEquals(1, pT.pythagoreanTriple(p1));
+	}
+
+	@Test
+	public void invalidTriple(){
+		assertEquals(0,pT.pythagoreanTriple(p2));
+	}
+
+}
