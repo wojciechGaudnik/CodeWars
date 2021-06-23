8acbb50ddefb0db9f6389ab5131889c0d559fb57
bq666 <wojciech.gaudnik@gmail.com>
2021-06-23 19:38:52 +0200

    next

---
+++ Java/src/main/java/suzuki_needs_help_lining_up_his_students/kyu7/Kata.java
@@ -0,0 +1,13 @@
+package suzuki_needs_help_lining_up_his_students.kyu7;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Kata {
+	public static String[] lineupStudents(String students){
+		return Arrays.stream(students.split(" "))
+				.sorted(Comparator.comparing(String::length)
+						.thenComparing(String::compareTo).reversed())
+				.toArray(String[]::new);
+	}
+}
