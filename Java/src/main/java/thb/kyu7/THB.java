0700f20453d0bace43ecab98f99b8a4a706a96fe
bq666 <wojciech.gaudnik@gmail.com>
2021-07-02 20:23:35 +0200

    next

--- Java/src/main/java/thb/kyu7/THB.java
+++ Java/src/main/java/thb/kyu7/THB.java
@@ -2,7 +2,7 @@ package thb.kyu7;
 
 public class THB {
 	public static String testing(String initial) {
-		if (initial.equals("")) {
+		if (initial == null) {
 			return "";
 		}
 		return initial.replaceAll("(?i)[^tbh]", "");
