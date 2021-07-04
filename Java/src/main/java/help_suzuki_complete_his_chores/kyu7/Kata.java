7a1f48c13c9b2c64f0d324192fb150f224267422
bq666 <wojciech.gaudnik@gmail.com>
2021-07-04 16:04:17 +0200

    next

---
+++ Java/src/main/java/help_suzuki_complete_his_chores/kyu7/Kata.java
@@ -0,0 +1,16 @@
+package help_suzuki_complete_his_chores.kyu7;
+
+import java.util.Arrays;
+
+public class Kata {
+
+	public static int[] choreAssignments(int[] chores){
+		Arrays.sort(chores);
+		var answer = new int[chores.length / 2];
+		for (int i = 0, j = chores.length - 1; i < j; i++, j--) {
+			answer[i] = chores[i] + chores[j];
+		}
+		Arrays.sort(answer);
+		return answer;
+	}
+}
