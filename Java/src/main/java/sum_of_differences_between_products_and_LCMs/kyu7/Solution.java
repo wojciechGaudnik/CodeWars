741dcc53d76de28d24ae2e660cbeed0ccf55def6
bq666 <wojciech.gaudnik@gmail.com>
2021-08-07 10:59:44 +0200

    next

--- Java/src/main/java/sum_of_differences_between_products_and_LCMs/kyu7/Solution.java
+++ Java/src/main/java/sum_of_differences_between_products_and_LCMs/kyu7/Solution.java
@@ -1,52 +1,26 @@
-//package sum_of_differences_between_products_and_LCMs.kyu7;
-//
-//import java.math.BigInteger;
-//import java.util.Arrays;
-//
-//public class Solution {
-//	public static int sumDifferencesBetweenProductsAndLCMs(final int[][] pairs) {
-//		System.out.println(Arrays.deepToString(pairs));
-//		System.out.println(lcm(15, 18));
-//		System.out.println(lcm(4, 5));
-//		System.out.println(lcm(12, 60));
-//		return Math.toIntExact(Arrays.stream(pairs).mapToInt(p ->
-//				BigInteger.valueOf(p[0]).multiply(BigInteger.valueOf(p[1])) - lcm(p[0], p[1])).sum());
-//
-//	}
-//
-//	public static int lcm(int number1, int number2) {
-//		if (number1 == 0 || number2 == 0)
-//			return 0;
-//		else {
-//			int gcd = gcd(number1, number2);
-//			return Math.abs(number1 * number2) / gcd;
-//		}
-//	}
-//
-//	public static int gcd(int number1, int number2) {
-//		if (number1 == 0 || number2 == 0) {
-//			return number1 + number2;
-//		} else {
-//			int biggerValue = Math.max(number1, number2);
-//			int smallerValue = Math.min(number1, number2);
-//			return gcd(biggerValue % smallerValue, smallerValue);
-//		}
-//	}
-//
-////	public static int findGCF(int num1, int num2) {
-////		if (num2 == 0) {
-////			return num1;
-////		} else {
-////			return findGCF(num2, num1 % num2);
-////		}
-////	}
-////
-////	public static int findLCM(int num1, int num2) {
-////		if (num1 == 0 || num2 == 0) {
-////			return 0;
-////		} else {
-////			var gcd = gcd(num1, num2);
-////			return Math.abs(num1 * num2) / gcd;
-////		}
-////	}
-//}
+package sum_of_differences_between_products_and_LCMs.kyu7;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static int sumDifferencesBetweenProductsAndLCMs(final int[][] pairs) {
+		return Math.toIntExact(Arrays.stream(pairs).mapToInt(p -> (p[0] * p[1]) - findLCM(p[0], p[1])).sum());
+	}
+
+	public static int findGCF(int num1, int num2) {
+		if (num2 == 0) {
+			return num1;
+		} else {
+			return findGCF(num2, num1 % num2);
+		}
+	}
+
+	public static int findLCM(int num1, int num2) {
+		if (num1 == 0 || num2 == 0) {
+			return 0;
+		} else {
+			var gcd = findGCF(num1, num2);
+			return (num1 * num2) / gcd;
+		}
+	}
+}
--- Java/src/test/java/sum_of_differences_between_products_and_LCMs/kyu7/SolutionTest.java
+++ Java/src/test/java/sum_of_differences_between_products_and_LCMs/kyu7/SolutionTest.java
@@ -1,18 +1,20 @@
-//package sum_of_differences_between_products_and_LCMs.kyu7;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class SolutionTest {
-//	@Test
-//	public void BasicTests() {
-////		assertEquals(840, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{15, 18}, {4, 5}, {12, 60}}));
-////		assertEquals(1092, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{1, 1}, {0, 0}, {13, 91}}));
-////		assertEquals(0, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{15, 7}, {4, 5}, {19, 60}}));
-////		assertEquals(1890, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{20, 50}, {10, 10}, {50, 20}}));
-////		assertEquals(0, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{}));
-//		assertEquals(571192496, Solution.sumDifferencesBetweenProductsAndLCMs(
-//				new int[][]{{130321, 122825}, {6137, 93347}, {175, 637}, {18, 14161}, {425, 6591}, {19652, 4225}, {93347, 189}}));
-//	}
-//}
+package sum_of_differences_between_products_and_LCMs.kyu7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+	@Test
+	public void BasicTests() {
+		assertEquals(840, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{15, 18}, {4, 5}, {12, 60}}));
+		assertEquals(1092, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{1, 1}, {0, 0}, {13, 91}}));
+		assertEquals(0, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{15, 7}, {4, 5}, {19, 60}}));
+		assertEquals(1890, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{20, 50}, {10, 10}, {50, 20}}));
+		assertEquals(0, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{}));
+		assertEquals(571192496, Solution.sumDifferencesBetweenProductsAndLCMs(
+				new int[][]{{130321, 122825}, {6137, 93347}, {175, 637}, {18, 14161}, {425, 6591}, {19652, 4225}, {93347, 189}}));
+		assertEquals(-941443727, Solution.sumDifferencesBetweenProductsAndLCMs(new int[][]{{9025, 37349}, {88, 614125}, {274625, 171}, {1444, 171475}, {54872, 614125}, {3211, 5324}, {49, 122825}, {44217, 274625}, {361, 171}, {8575, 857375}}));
+	}
+}
+
