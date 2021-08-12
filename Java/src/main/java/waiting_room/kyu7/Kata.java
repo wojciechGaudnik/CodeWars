4074a9cccb75978f13eb863f1b2d7966cfd472e6
bq666 <wojciech.gaudnik@gmail.com>
2021-08-12 08:40:25 +0200

    next

---
+++ Java/src/main/java/waiting_room/kyu7/Kata.java
@@ -0,0 +1,7 @@
+package waiting_room.kyu7;
+
+public class Kata {
+	public static int lastChair(int n) {
+		return n - 1;
+	}
+}
---
+++ Java/src/test/java/waiting_room/kyu7/KataTest.java
@@ -0,0 +1,12 @@
+package waiting_room.kyu7;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KataTest {
+	@Test
+	public void tests() {
+		assertEquals(9, Kata.lastChair(10));
+	}
+}
