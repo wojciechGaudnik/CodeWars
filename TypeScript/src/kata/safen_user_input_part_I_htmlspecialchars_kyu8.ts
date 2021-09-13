c5f4dca73cb1214770ea49e31c608e5617ef91f3
bq666 <wojciech.gaudnik@gmail.com>
2021-09-13 15:49:13 +0200

    next

---
+++ TypeScript/src/kata/safen_user_input_part_I_htmlspecialchars_kyu8.ts
@@ -0,0 +1,8 @@
+
+export function htmlspecialchars(formData: string): string {
+    return formData
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+}
