22a14843a4420a6d58a7008d3a3387bad4c27764
bq666 <wojciech.gaudnik@gmail.com>
2021-09-19 14:57:19 +0200

    next

---
+++ TypeScript/src/kata/learning_typeScript_basic_types_kyu8.ts
@@ -0,0 +1,16 @@
+export enum Color {Blue,Red}
+export var var1Boolean: boolean = true;
+export var var2Decimal: number = 13;
+export var var3Hex: number = 0xf00d;
+export var var4Binary: number = 0b111111;
+export var var5Octal: number = 0o744;
+export var var6String: string = 'Hello, world!';
+export var var7Array: any[] = [1, 'test', {a: 3}, 4, 5];
+export var var8NumericArray: number[] = [1, 2, 3, 4, 5];
+export var var9Tuple: any[] = ['key', 12345];
+export var var10Enum: Color = Color.Blue;
+export var var11ArrayOfAny: any[];
+export function var12VoidFunction(){}
+export var var13Null: null = null
+export var var14Undefined: undefined = undefined
+export function var15NeverFunction(){}
