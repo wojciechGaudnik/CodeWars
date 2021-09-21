3710adbd3bc564baa8a9b827214bfb3d5875f56d
bq666 <wojciech.gaudnik@gmail.com>
2021-09-21 15:11:08 +0200

    next

--- Bash/ string_reversing_changing_case_etc_kyu7.sh
+++ Bash/ string_reversing_changing_case_etc_kyu7.sh
@@ -1,6 +1,25 @@
-answer=''
-for ((i=0;i<${#$1};i++))
-do
-  echo $1:$i:1
+#!/usr/bin/env bash
+first=$(echo $1 |rev)
+first+=$1
+second=$(echo $2 |rev)
+upperSecond=${second^^}
+lowerSecond=${second,,}
+upperFirst=${first^^}
+lowerFirst=${first,,}
+answer=""
+for (( i = 0; i < ${#second}; i++ )); do
+  if [[ ${second:i:1} == [A-Z] ]]; then
+    answer=$answer"${lowerSecond:i:1}"
+  else
+    answer=$answer"${upperSecond:i:1}"
+  fi
+done
+answer+="@@@"
+for (( i = 0; i < ${#first}; i++ )); do
+  if [[ ${first:i:1} == [A-Z] ]]; then
+    answer=$answer"${lowerFirst:i:1}"
+  else
+    answer=$answer"${upperFirst:i:1}"
+  fi
 done
 echo $answer
--- Bash/reverse_kyu7.sh
+++ Bash/reverse_kyu7.sh
@@ -1,31 +1,4 @@
 #!/usr/bin/sh
-#copy=$1
-#len=${#copy}
-#for ((i=$len-1;i>=0;i--));
-#do
-#    rev="$rev${copy:$i:1}"
-#done
-#set rev
-#foreach s ( $1 )
-#  rev=$s + $rev
-#end
-#echo $rev
-
-#old=00123
-#echo $old
-#printf %10d $1|rev
-echo "---"
-a=$(echo $1|rev)
-b=$(echo $a | sed 's/^0*//')
-echo $b
-
-#echo $(echo $1|rev)
-echo $1|rev| sed 's/^0*//'
-
-echo "---"
-c=$(echo $1|rev)
-
-echo "3333333333-------"
 x=$(echo $1|rev)
 echo $x | awk '{$0=int($0)}1'
-http://underpop.online.fr/u/unix-school/guru-prasad/how-to-remove-leading-zeros-in-string.htm
+#http://underpop.online.fr/u/unix-school/guru-prasad/how-to-remove-leading-zeros-in-string.htm
