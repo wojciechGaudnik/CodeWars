6fb3d891f0c0417bdce8019c755bc8e78a270f07
bq666 <wojciech.gaudnik@gmail.com>
2021-09-25 15:15:06 +0200

    next

--- Bash/floating_point_approximation_III_kyu7.sh
+++ Bash/floating_point_approximation_III_kyu7.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 function quadratic() {
-  #  bc <<< "scale=$1; -$3 / $2"
-  echo "a = $3"
-  echo "b = $3"
-  echo "c = $3"
-  bc <<<"scale=10; -$3 / $2"
+  bc <<<"scale=26;-$3 / $2"
 }
 quadratic $1 $2 $3
