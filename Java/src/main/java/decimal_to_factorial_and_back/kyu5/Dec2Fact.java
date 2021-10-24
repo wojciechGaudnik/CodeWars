6774a4699dced159a1d823dc7453a81d4bd0e688
bq666 <wojciech.gaudnik@gmail.com>
2021-10-24 21:59:01 +0200

    next

--- Java/src/main/java/decimal_to_factorial_and_back/kyu5/Dec2Fact.java
+++ Java/src/main/java/decimal_to_factorial_and_back/kyu5/Dec2Fact.java
@@ -1,2 +1,25 @@
-package decimal_to_factorial_and_back.kyu5;class Dec2Fact {
+package decimal_to_factorial_and_back.kyu5;
+
+class Dec2Fact {
+	private static String base = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static String dec2FactString(long nb) {
+		var answer = new StringBuilder("0");
+		for (var i = 2; nb > 0; nb /= i++) {
+			answer.insert(0, base.charAt((int) (nb % i)));
+		}
+		return answer.toString();
+	}
+
+	public static long factString2Dec(String str) {
+		long answer = 0;
+		long multiplier = 1;
+		for (int i = 0, j = str.length() - 1; j >= 0; i++, j--) {
+			if (i > 1) {
+				multiplier *= i;
+			}
+			answer += multiplier * base.indexOf(str.charAt(j));
+		}
+		return answer;
+	}
 }
--- Java/src/test/java/decimal_to_factorial_and_back/kyu5/Dec2FactTest.java
+++ Java/src/test/java/decimal_to_factorial_and_back/kyu5/Dec2FactTest.java
@@ -1,6 +1,22 @@
 package decimal_to_factorial_and_back.kyu5;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 public class Dec2FactTest {
-  
+	@Test
+	public void test11() {
+		assertEquals(Dec2Fact.dec2FactString(1001L), "1212210");
+	}
+
+//	@Test
+//	public void test12() {
+//		assertEquals(Dec2Fact.factString2Dec("2DCAA5842344512200"),1000000000000000L );
+//	}
+
+
+	@Test
+	public void test13() {
+		assertEquals(Dec2Fact.factString2Dec("17747074033000"),9876543210L);
+	}
 }
