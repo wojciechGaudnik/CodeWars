faf9e3def5855ee93ca1672c235ca3257dd8ff55
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-11-19 16:06:10 +0100

    next

---
+++ JavaScript/kyu8_TrainingJSBasicDataTypesString/kataTest.js
@@ -0,0 +1,7 @@
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertSimilar(Dad(),"Dad");
+        Test.assertSimilar(Bee(),"Bee");
+        Test.assertSimilar(banana(),"banana");
+    });
+});
