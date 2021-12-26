f7187d155896341f30471ff04051d069f7cd3c9b
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-12-26 09:39:19 +0100

    next

---
+++ JavaScript/kyu8_ AspectRatioCroppingPart1/kataTest.js
@@ -0,0 +1,8 @@
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertSimilar(aspectRatio(640, 480), [854,480]);
+        Test.assertSimilar(aspectRatio(960, 720), [1280,720]);
+        Test.assertSimilar(aspectRatio(1440, 1080), [1920,1080]);
+        Test.assertSimilar(aspectRatio(1920, 1440), [2560,1440]);
+    });
+});
