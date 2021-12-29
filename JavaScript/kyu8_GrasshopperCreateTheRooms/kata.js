16bcffc36a46d2e96b09ec8772aef8d06665c9ad
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2021-12-29 18:29:37 +0100

    next

---
+++ JavaScript/kyu8_GrasshopperCreateTheRooms/kata.js
@@ -0,0 +1,17 @@
+var rooms = {
+    room1: {
+        size: 1,
+        high: 2,
+        windows: 3
+    },
+    room2: {
+        size: 1,
+        high: 2,
+        windows: 3
+    },
+    room3: {
+        size: 1,
+        high: 2,
+        windows: 3
+    }
+}
