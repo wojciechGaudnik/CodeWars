c8792ced265b120357bbbbb893ad229b9ac9a9d1
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-01-01 16:17:27 +0100

    next

---
+++ JavaScript/kyu8_SimpleFun352ReagentFormula/kata.js
@@ -0,0 +1,7 @@
+function isValid(formula) {
+    return !((formula.includes(1) && formula.includes(2)) ||
+        (formula.includes(3) && formula.includes(4)) ||
+        (formula.includes(5) && !formula.includes(6)) ||
+        (!formula.includes(5) && formula.includes(6)) ||
+        (!formula.includes(7) && !formula.includes(8)));
+}
