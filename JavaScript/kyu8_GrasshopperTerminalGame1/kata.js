b70af5466ef78866d48efa4713acb0a25c8f4f0b
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-01-28 20:39:11 +0100

    next

---
+++ JavaScript/kyu8_GrasshopperTerminalGame1/kata.js
@@ -0,0 +1,9 @@
+class Hero {
+    constructor(name) {
+        this.name = name || 'Hero',
+        this.position = '00',
+        this.health = 100,
+        this.damage = 5,
+        this.experience = 0
+    }
+}
