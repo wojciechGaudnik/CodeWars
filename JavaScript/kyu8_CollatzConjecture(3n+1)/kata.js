fa311d7b0ed823bcee990c55d408edabf06b8e0a
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-01-29 13:18:35 +0100

    next

---
+++ JavaScript/kyu8_CollatzConjecture(3n+1)/kata.js
@@ -0,0 +1,16 @@
+var hotpo = function (n) {
+    var answer = 0;
+    while (n > 1) {
+        n = func(n)
+        answer++;
+    }
+    return answer;
+
+}
+
+let func = function (number) {
+    if (number % 2 === 0) {
+        return number / 2
+    }
+    return (3 * number) + 1
+}
---
+++ JavaScript/kyu8_CollatzConjecture(3n+1)/kataTest.js
@@ -0,0 +1,8 @@
+describe("Test", function(){
+    it("Examples", function(){
+        Test.assertEquals(hotpo(1), 0);
+        Test.assertEquals(hotpo(5), 5);
+        Test.assertEquals(hotpo(6), 8);
+        Test.assertEquals(hotpo(23), 15);
+    });
+});
