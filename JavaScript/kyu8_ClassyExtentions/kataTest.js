f10e544d0406bf05e775094d7f40c6712cab2ce3
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-01-31 17:32:45 +0100

    next

---
+++ JavaScript/kyu8_ClassyExtentions/kataTest.js
@@ -0,0 +1,8 @@
+describe('Fixed Tests', () => {
+    var cat = new Cat('Mr Whiskers');
+    Test.assertEquals(cat.speak(),'Mr Whiskers meows.')
+    cat = new Cat('Lamp');
+    Test.assertEquals(cat.speak(),'Lamp meows.')
+    cat = new Cat('$$Money Bags$$');
+    Test.assertEquals(cat.speak(),'$$Money Bags$$ meows.')
+});
