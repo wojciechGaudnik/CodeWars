2fb0df2d5ab691569dfcdd01039432867e3db5c0
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-02-17 16:22:55 +0100

    next

---
+++ JavaScript/kyu8_SimpleComparison/kataTest.js
@@ -0,0 +1,10 @@
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(add('1', 1), true);
+        Test.assertEquals(add(1, '1'), true);
+        Test.assertEquals(add(1, '0'), false);
+        Test.assertEquals(add('11', 11), true);
+        Test.assertEquals(add(12, 12), true);
+        Test.assertEquals(add(120, '021'), false);
+    });
+});
