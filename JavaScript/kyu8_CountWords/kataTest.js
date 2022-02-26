fe6b942e97c3c927bbc43c4b7c7a4579028cd9a7
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-02-26 12:47:51 +0100

    next

--- JavaScript/kyu8_CountWords/kataTest.js
+++ JavaScript/kyu8_CountWords/kataTest.js
@@ -1,17 +1,23 @@
 describe("countWords", function() {
     it("should work in basic form of problem", function() {
-        "Hello"), 1);
-        "Hello, World!"), 2);
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), 19);
-        ""), 0);
-        "With! Symbol@ #Around! (Every) %Word$"), 5);
-        "Dear   Victoria, I love  to press   space button."), 8);
-        " Arthur "), 1);
-        " David"), 1);
-        "Nelson "), 1);
-        "  Hello Gomer  "), 3);
-        "  Hello     Bart  "), 2);
-        "﻿Hello﻿World "), 2);
-        "Hello﻿World"), 2);
+        Test.assertEquals(countWords("Hello"), 1);
+        Test.assertEquals(countWords("Hello, World!"), 2);
+        Test.assertEquals(countWords("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), 19);
+        Test.assertEquals(countWords(""), 0);
+        Test.assertEquals(countWords("With! Symbol@ #Around! (Every) %Word$"), 5);
+        Test.assertEquals(countWords("Dear   Victoria, I love  to press   space button."), 8);
+    });
+
+    it("should work with spaces around string", function() {
+        Test.assertEquals(countWords(" Arthur "), 1);
+        Test.assertEquals(countWords(" David"), 1);
+        Test.assertEquals(countWords("Nelson "), 1);
+        Test.assertEquals(countWords("  Hello Gomer  "), 3);
+        Test.assertEquals(countWords("  Hello     Bart  "), 2);
+    });
+
+    it("should work with non-whitespace (ex. breakspace) chars", function() {
+        Test.assertEquals(countWords("﻿Hello﻿World "), 2);
+        Test.assertEquals(countWords("Hello﻿World"), 2);
     });
 });
