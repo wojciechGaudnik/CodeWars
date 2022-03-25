fef9ab28bfa3c0c915cf876b7c3c2ef14065f231
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-03-25 15:56:59 +0100

    next

---
+++ JavaScript/kyu8_noobCode02TRICKYQUESTIONSprimitivesAndOperatorPrecedence/kataTest.js
@@ -0,0 +1,8 @@
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(greaterThanLessThan(Number(null), 0, 1),true);
+        Test.assertEquals(greaterThanLessThan(700000000001, 700000000002, -1),false);
+        Test.assertEquals(greaterThanLessThan(-9, -8, 7),true);
+        Test.assertEquals(greaterThanLessThan(-9, -8, -7),false);
+    });
+});
