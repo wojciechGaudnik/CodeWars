49b06006c6d347c09764d04cbc89399fa255b387
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-05-05 12:24:25 +0200

    next

---
+++ Java/src/main/java/series_of_integers_from_m_to_n/kyu7/Kata/Kata.java
@@ -0,0 +1,12 @@
+package series_of_integers_from_m_to_n.kyu7.Kata;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Kata {
+
+	public static int[] generateIntegers(final int m, final int n) {
+		return IntStream.range(m, n + 1).toArray();
+	}
+
+}
---
+++ Java/src/main/java/series_of_integers_from_m_to_n/kyu7/Kata/SampleTest.java
@@ -0,0 +1,12 @@
+//package series_of_integers_from_m_to_n.kyu7.Kata;
+//
+//import org.junit.Test;
+//
+//// TODO: Replace examples and use TDD by writing your own tests
+//
+//public class SampleTest {
+//	@Test
+//	public void exampleTests() {
+//		Tester.doTest(2, 5, new int[]{2, 3, 4, 5});
+//	}
+//}
