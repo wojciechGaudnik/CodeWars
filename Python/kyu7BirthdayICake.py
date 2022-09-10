e7a624b6645f4b5d0d6103dcd6f19d5641cd8722
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-09-10 14:46:21 +0200

    next

---
+++ Python/kyu7BirthdayICake.py
@@ -0,0 +1,9 @@
+def cake(candles, debris):
+    if candles == 0:
+        return 'That was close!'
+    sum_of_candles = 0
+    for i in range(0, len(debris), 2):
+        sum_of_candles += ord(debris[i])
+    for i in range(1, len(debris), 2):
+        sum_of_candles += ord(debris[i]) - 96
+    return 'That was close!' if sum_of_candles < candles * 0.7 else 'Fire!'
