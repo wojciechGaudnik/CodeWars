b528205422f8f72332c6c7baeac218b62ad7dc7d
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2022-12-17 09:00:42 +0100

    next

---
+++ Python/kyu7FizzBuzz2.py
@@ -0,0 +1,11 @@
+def solution(number):
+    answer = [0, 0, 0]
+    A, B, C = 0, 1, 2
+    for n in range(1, number):
+        if n % 3 == 0 and n % 5 == 0:
+            answer[C] += 1
+        elif n % 3 == 0 and n % 5 != 0:
+            answer[A] += 1
+        elif n % 5 == 0 and n % 3 != 0:
+            answer[B] += 1
+    return answer
