df65f8ad3f6fccc7172982da775ecacf820f7754
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2023-01-14 14:02:17 +0100

    next

--- Python/kyu7TheAlphabetProduct.py
+++ Python/kyu7TheAlphabetProduct.py
@@ -1,25 +1,24 @@
 def alphabet(ns):
-    numbers = list(sorted(set(ns)))[0:4]
-    print(numbers)
-    print(sorted(ns))
-    products = sorted(ns)[3:5]
-    print(products)
-    if numbers[0] * numbers[1] in numbers:
-        return products[1]
-    return products[0]
-
-
-print(alphabet([13, 338, 11336, 26, 13, 436, 1, 436]), 436)
-print("--------")
-print(alphabet([2, 3, 4, 1, 12, 6, 2, 4]), 4)
-print("--------")
-# print(alphabet([2, 6, 7, 3, 14, 35, 15, 5]), 7)
-# print("--------")
-# print(alphabet([20, 10, 6, 5, 4, 3, 2, 12]), 5)
-# print("--------")
-# print(alphabet([2, 6, 18, 3, 6, 7, 42, 14]), 7)
-# print("--------")
-# print(alphabet([7, 96, 8, 240, 12, 140, 20, 56]), 20)
-# print("--------")
-# print(alphabet([20, 30, 6, 7, 4, 42, 28, 5]), 7)
-# print("--------")
+    numbers = sorted(set(ns))
+    products = sorted(ns)
+    a = min(numbers)
+    cd = max(products)
+    numbers.remove(a)
+    numbers.remove(cd)
+    products.remove(a)
+    products.remove(cd)
+    b = min(numbers)
+    ab = a * b
+    numbers.remove(b)
+    if ab in numbers:
+        numbers.remove(ab)
+    products.remove(b)
+    products.remove(ab)
+    if numbers == products:
+        return numbers[1]
+    if products[-1] == products[-2]:
+        return products[-1]
+    if numbers == products[1:]:
+        return numbers[0]
+    if numbers == sorted(set(products)):
+        return numbers[1]
