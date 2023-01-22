c2f9276e48c5874dc20f9dcf7be6eb0c724ac4aa
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2023-01-22 12:02:53 +0100

    next

---
+++ Python/kyu7DoubleTrouble.py
@@ -0,0 +1,10 @@
+def trouble(x, t):
+    start_index = 0
+    stop_index = 1
+    while stop_index <= len(x) - 1:
+        if x[start_index] + x[stop_index] == t:
+            x.pop(stop_index)
+        elif x[start_index] + x[stop_index] != t:
+            start_index += 1
+            stop_index = start_index + 1
+    return x
