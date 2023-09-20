daff7d371f5b89c1fe876d5256ef7ae7a53be83f
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2023-09-20 11:46:22 +0200

    next

--- Java/src/test/java/quadrants/kata8/KataTest.java
+++ Java/src/test/java/quadrants/kata8/KataTest.java
@@ -1,12 +1,21 @@
 package quadrants.kata8;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KataTest {
 
-    @Test
-    void quadrant() {
+    @ParameterizedTest(name = "Quadrant {0}: ({1}, {2})")
+    @CsvSource(textBlock = """
+        1,       1,   2
+        1,       3,   5
+        2,     -10, 100
+        3,      -1,  -9
+        4,      19, -56
+    """)
+    @DisplayName("Sample tests") void sampleTests(int expected, int x, int y) {
+        assertEquals(expected, Kata.quadrant(x, y));
     }
 }
